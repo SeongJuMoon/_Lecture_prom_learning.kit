ea30261bd08fec5ad2cedddf1a71944be073e90f
Seongju Moon <seongju.moon@linux.com>
2021-08-03 00:03:08 +0900

    [WIP] prometheus kubernetes lab

---
+++ lesson03/install_pkg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# install packages 
+yum install epel-release -y
+yum install vim-enhanced -y
+yum install git -y
+
+# install docker 
+yum install docker-ce-20.10.7-3.el7 docker-ce-cli-20.10.7-3.el7 \
+    containerd.io-1.4.6-3.1.el7 -y
+systemctl enable --now docker
+
+# install kubernetes cluster 
+yum install kubectl-$1 kubelet-$1 kubeadm-$1 -y
+systemctl enable --now kubelet
+
+# git clone _Book_k8sInfra.git 
+if [ $2 = 'Main' ]; then
+  git clone https://github.com/sysnet4admin/_Book_k8sInfra.git
+  mv /home/vagrant/_Book_k8sInfra $HOME
+  find $HOME/_Book_k8sInfra/ -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+fi

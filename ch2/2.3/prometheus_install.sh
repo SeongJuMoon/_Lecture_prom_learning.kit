331ef82f47a8dabaf697daeb608d978b922cdde7
seongjumoon <seongjumoon@linux.com>
2022-05-07 18:00:36 +0900

    mend

--- ch2/2.3/prometheus_install.sh
+++ ch2/2.3/prometheus_install.sh
@@ -8,7 +8,7 @@ rm -rf prometheus-2.30.0.linux-amd64.tar.gz
 install -m 0755 $TMPDIR/prometheus $TMPDIR/promtool /usr/bin/
 install -d -o root -g root /var/lib/prometheus
 install -m 0644 -D $TMPDIR/prometheus.yml /etc/prometheus/prometheus.yml
-install -m 0644 -D $HOME/config/prometheus.service /etc/systemd/system
+install -m 0644 -D $HOME/_Lecture_prometheus_learning.kit/ch2/2.3/config/prometheus.service /etc/systemd/system
 
 systemctl daemon-reload
 systemctl enable --now prometheus
--- ch2/2.4/node_exporter_install.sh
+++ ch2/2.4/node_exporter_install.sh
@@ -2,7 +2,7 @@
 
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvzf node_exporter-1.3.1.linux-amd64.tar.gz -C /usr/bin --strip-components=1
-cp $HOME/config/node-exporter.service  /etc/systemd/system
+cp $HOME/_Lecture_prometheus_learning.kit/ch2/2.4/configs/node_exporter.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable --now node-exporter
 systemctl start node-exporter

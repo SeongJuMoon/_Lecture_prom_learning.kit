35d49c96cf46bf89de6200bf25528301a4d29a53
Hoon Jo <pagaia@hotmail.com>
2022-07-16 23:18:21 +0900

    upt sleep in 300

--- ch2/2.2/essential-k8s-pkgs-installer.sh
+++ ch2/2.2/essential-k8s-pkgs-installer.sh
@@ -8,6 +8,8 @@ sh $SCRIPTSDIR/get-helm-3.9.1.sh
 
 # metallb v0.13.3
 kubectl apply -f $SCRIPTSDIR/metallb-native-v0.13.3.yaml
+# config metallb ip range and it cannot deploy now due to CRD cannot create yet 
+(sleep 300 && kubectl apply -f $SCRIPTSDIR/metallb-iprange.yaml)&
 
 # metrics server v0.6.1 - insecure mode 
 kubectl apply -f  $SCRIPTSDIR/metrics-server-0.6.1.yaml
@@ -24,5 +26,3 @@ kubectl apply -f $SCRIPTSDIR/storageclass.yaml
 # setup default storage class due to no mention later on
 kubectl annotate storageclass managed-nfs-storage storageclass.kubernetes.io/is-default-class=true
 
-# config metallb ip range and it cannot deploy with metallb due to CRD cannot create yet 
-(sleep 300 && kubectl apply -f $SCRIPTSDIR/metallb-iprange.yaml)&

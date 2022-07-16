90a6a09edbbdc2ec47f6ef51eff0508e79018ab7
Hoon Jo <pagaia@hotmail.com>
2022-07-16 20:58:31 +0900

    upt extra_k8s_pkg

--- ch2/2.2/scripts-to-deploy/helm-repo-add.sh
+++ ch2/2.2/scripts-to-deploy/helm-repo-add.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # instant shlle cannot read properly env. so temperally full address fixed 
-export PATH=$PATH:/usr/local/bin/
+#export PATH=$PATH:/usr/local/bin/
+
+cp /usr/local/bin/helm /usr/bin/helm
+
 helm repo add edu https://k8s-edu.github.io/helm-charts
 helm repo update
 helm completion bash >/etc/bash_completion.d/helm

ba6caaf923c3c5a0f110bca2a9dbefa7bbf7c44a
Hoon Jo <pagaia@hotmail.com>
2022-07-16 21:59:04 +0900

    upt essential k8s pkgs

--- ch2/2.2/essential-k8s-pkgs-installer.sh
+++ ch2/2.2/essential-k8s-pkgs-installer.sh
@@ -7,16 +7,17 @@ SCRIPTSDIR=$HOME/_Lecture_prometheus_learning.kit/ch2/2.2/essential-k8s-pkgs
 sh $SCRIPTSDIR/get-helm-3.9.1.sh
 
 # metallb v0.13.3
-sh $SCRIPTSDIR/metallb-installer-0.13.3-w-CRD.sh
+kubectl apply -f $SCRIPTSDIR/metallb-native-v0.13.3.yaml
+kubectl apply -f $SCRIPTSDIR/metallb-iprange.yaml
 
 # metrics server v0.6.1
-sh $SCRIPTSDIR/metrics-server-0.6.1.sh
+kubectl apply -f  $SCRIPTSDIR/metrics-server-0.6.1.yaml
 
 # NFS dir configuration
 sh $SCRIPTSDIR/nfs-exporter.sh dynamic-vol
 
 # nfs-provsioner installer 
-sh $SCRIPTSDIR/nfs-provisioner-installer.sh
+kubectl apply -f  $SCRIPTSDIR/nfs-provisioner.yaml
 
 # storageclass installer & set default storageclass
 sh $SCRIPTSDIR/storageclass-intaller.sh

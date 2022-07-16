fef0436df223d353ba1ae2cfc40d76753df68f96
Hoon Jo <pagaia@hotmail.com>
2022-07-16 18:52:49 +0900

    chg metallb-l2config loc

--- ch2/2.2/scripts-by-helm/metallb-installer-0.13.3.sh
+++ ch2/2.2/scripts-by-helm/metallb-installer-0.13.3.sh
@@ -5,4 +5,4 @@ helm install metallb edu/metallb \
      --namespace=metallb-system \
      --set controller.image.tag=v0.13.3 \
      --set speaker.image.tag=v0.13.3 \
-     -f ~/_Lecture_prometheus_learning.kit/ch2/2.2/provisionning/l2-metallb-config.yaml
+     -f ~/_Lecture_prometheus_learning.kit/ch2/2.2/scripts-by-helm/l2-metallb-config.yaml

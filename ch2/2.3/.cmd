da02d2ff4c139f851df12aa25b782cd266059d95
Seongju Moon <seongju.moon@linux.com>
2022-07-18 01:49:51 +0900

    enhanced

---
+++ ch2/2.3/.cmd
@@ -0,0 +1,2 @@
+~/_Lecture_prometheus_learning.kit/ch2/2.3/helm-repo-add.sh 
+~/_Lecture_prometheus_learning.kit/ch2/2.3/prometheus-installer.sh
--- ch2/2.3/prometheus-installer.sh
+++ ch2/2.3/prometheus-installer.sh
@@ -5,7 +5,6 @@ helm install prometheus edu/prometheus \
 --set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
 --set nodeExporter.tolerations[0].effect="NoSchedule" \
 --set nodeExporter.tolerations[0].operator="Exists" \
---set server.persistentVolume.storageClass="managed-nfs-storage" \
 --set server.service.type="LoadBalancer" \
 --namespace=monitoring \
 --create-namespace

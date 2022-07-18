f2d352ea80e81081c5dc9c4299f5a2eecb9c1f25
Seongju Moon <seongju.moon@linux.com>
2022-07-19 08:18:36 +0900

    change toleration

--- ch2/2.3/prometheus-installer.sh
+++ ch2/2.3/prometheus-installer.sh
@@ -2,6 +2,9 @@
 
 helm install prometheus edu/prometheus \
 --set pushgateway.enabled=false \
+--set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
+--set nodeExporter.tolerations[0].effect="NoSchedule" \
+--set nodeExporter.tolerations[0].operator="Exists" \
 --set nodeExporter.tolerations[0].key="node-role.kubernetes.io/control-plane" \
 --set nodeExporter.tolerations[0].effect="NoSchedule" \
 --set nodeExporter.tolerations[0].operator="Exists" \

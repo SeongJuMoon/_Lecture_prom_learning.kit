dc5bc4a5c426ce419b8c7b9a9d52f592d87f7457
Seongju Moon <seongju.moon@linux.com>
2022-07-19 08:20:51 +0900

    fix wrong index.

--- ch2/2.3/prometheus-installer.sh
+++ ch2/2.3/prometheus-installer.sh
@@ -5,9 +5,9 @@ helm install prometheus edu/prometheus \
 --set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
 --set nodeExporter.tolerations[0].effect="NoSchedule" \
 --set nodeExporter.tolerations[0].operator="Exists" \
---set nodeExporter.tolerations[0].key="node-role.kubernetes.io/control-plane" \
---set nodeExporter.tolerations[0].effect="NoSchedule" \
---set nodeExporter.tolerations[0].operator="Exists" \
+--set nodeExporter.tolerations[1].key="node-role.kubernetes.io/control-plane" \
+--set nodeExporter.tolerations[1].effect="NoSchedule" \
+--set nodeExporter.tolerations[1].operator="Exists" \
 --set server.service.type="LoadBalancer" \
 --namespace=monitoring \
 --create-namespace

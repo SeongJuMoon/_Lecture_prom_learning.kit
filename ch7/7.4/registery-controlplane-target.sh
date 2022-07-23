1b67e1b1942d25c94ffb153ff255f6e18ea1ac01
Seongju Moon <seongju.moon@linux.com>
2022-07-23 14:02:55 +0900

    refactor: shell

--- ch7/7.4/registery-controlplane-target.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create -f ~/_Lecture_prometheus_learning.kit/ch7/7.3/kubernetesControlPlane-serviceMonitorApiserver.yaml
-kubectl create -f ~/_Lecture_prometheus_learning.kit/ch7/7.3/kubernetesControlPlane-serviceMonitorCoreDNS.yaml
-kubectl create -f ~/_Lecture_prometheus_learning.kit/ch7/7.3/kubernetesControlPlane-serviceMonitorKubeControllerManager.yaml
-kubectl create -f ~/_Lecture_prometheus_learning.kit/ch7/7.3/kubernetesControlPlane-serviceMonitorKubelet.yaml
-kubectl create -f ~/_Lecture_prometheus_learning.kit/ch7/7.3/kubernetesControlPlane-serviceMonitorKubeScheduler.yaml

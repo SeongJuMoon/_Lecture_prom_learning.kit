f89c2db5f641a75161e7f35cc3d4cdb73d9d014a
Seongju Moon <seongju.moon@linux.com>
2022-07-31 10:44:34 +0900

    refactor code.

--- ch7/7.4/register-monitor-target.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create -f ~/_Lecture_prom_learning.kit/ch7/7.4/kubernetesControlPlane-serviceMonitorApiserver.yaml
-kubectl create -f ~/_Lecture_prom_learning.kit/ch7/7.4/kubernetesControlPlane-serviceMonitorCoreDNS.yaml
-kubectl create -f ~/_Lecture_prom_learning.kit/ch7/7.4/kubernetesControlPlane-serviceMonitorKubeControllerManager.yaml
-kubectl create -f ~/_Lecture_prom_learning.kit/ch7/7.4/kubernetesControlPlane-serviceMonitorKubelet.yaml
-kubectl create -f ~/_Lecture_prom_learning.kit/ch7/7.4/kubernetesControlPlane-serviceMonitorKubeScheduler.yaml

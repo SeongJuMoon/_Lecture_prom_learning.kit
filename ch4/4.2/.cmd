548004eaa55bf6574ec7cd40ae3b7e16b17170bd
Hoon Jo <pagaia@hotmail.com>
2022-08-03 16:48:04 +0900

    upt cAdvisor lab

---
+++ ch4/4.2/.cmd
@@ -0,0 +1,9 @@
+# input this command on prometheus web ui
+container_network_receive_bytes_total
+
+container_network_receive_bytes_total{pod="chk-hn"}
+
+k apply -f po-chk-hn.yaml 
+
+curl-get.sh <IP>
+

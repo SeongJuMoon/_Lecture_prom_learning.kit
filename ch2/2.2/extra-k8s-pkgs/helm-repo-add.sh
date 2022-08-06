3364ff78c6c302319e2e7d17daba20222ec058a5
Hoon Jo <pagaia@hotmail.com>
2022-08-07 07:49:46 +0900

    chg repo addr

--- ch2/2.2/extra-k8s-pkgs/helm-repo-add.sh
+++ ch2/2.2/extra-k8s-pkgs/helm-repo-add.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-helm repo add edu https://k8s-edu.github.io/helm-charts
+helm repo add edu https://k8s-edu.github.io/helm-charts/prom
 helm repo update
 
 # helm auto-completion 

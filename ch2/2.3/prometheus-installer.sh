0d163bf91b09615edc1f9836d148f3fdc80e708b
Hoon Jo <pagaia@hotmail.com>
2022-08-07 08:25:02 +0900

    dis alertmanager

--- ch2/2.3/prometheus-installer.sh
+++ ch2/2.3/prometheus-installer.sh
@@ -2,6 +2,7 @@
 
 helm install prometheus edu/prometheus \
 --set pushgateway.enabled=false \
+--set alertmanager.enabled=false \
 --set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
 --set nodeExporter.tolerations[0].effect="NoSchedule" \
 --set nodeExporter.tolerations[0].operator="Exists" \
@@ -9,9 +10,6 @@ helm install prometheus edu/prometheus \
 --set nodeExporter.tolerations[1].effect="NoSchedule" \
 --set nodeExporter.tolerations[1].operator="Exists" \
 --set server.service.type="LoadBalancer" \
---set server.service.loadBalancerIP="192.168.1.11" \
---set alertmanager.service.type="LoadBalancer" \
---set alertmanager.service.loadBalancerIP="192.168.1.12" \
 --set server.extraFlags[1]="storage.tsdb.no-lockfile" \
 --namespace=monitoring \
 --create-namespace

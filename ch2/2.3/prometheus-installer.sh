230bb0271d96c1407f8783b52fe797191ce3a656
Seongju Moon <seongju.moon@linux.com>
2022-08-07 02:39:41 +0900

    fix wrong parameter set

--- ch2/2.3/prometheus-installer.sh
+++ ch2/2.3/prometheus-installer.sh
@@ -13,6 +13,6 @@ helm install prometheus edu/prometheus \
 --set alertmanager.service.type="LoadBalancer" \
 --set alertmanager.service.loadBalancerIP="192.168.1.12" \
 --set server.extraFlags[0]="storage.tsdb.no-lockfile" \
---set server.extraFlags[1]="--web.enable-lifecycle" \
+--set server.extraFlags[1]="web.enable-lifecycle" \
 --namespace=monitoring \
 --create-namespace

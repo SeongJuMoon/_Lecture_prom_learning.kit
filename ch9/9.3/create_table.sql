1540f9344d3dabc93ebe1f5d0078abd7dea247ba
Seongju Moon <seongju.moon@linux.com>
2022-08-11 02:26:00 +0900

    update sql

--- ch9/9.3/create_table.sql
+++ ch9/9.3/create_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE session_record (id int AUTO_INCREMENT,trace_id text,access_token text,user_uuid text, PRIMARY KEY (`id`))
+CREATE TABLE session_record (id int AUTO_INCREMENT, trace_id text, access_token text, user_uuid text, PRIMARY KEY (`id`))

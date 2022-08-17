4911a5ba4c80df5865f61cab2f9f9b7c41f37d77
Seongju Moon <seongju.moon@linux.com>
2022-08-18 04:14:52 +0900

    refactor mysql labs

--- ch9/9.3/install-mysql-client.sh
+++ ch9/9.3/install-mysql-client.sh
@@ -2,7 +2,4 @@
 
 sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 echo "current active mysql version is 8.0"
-# workaround 
-sed s,gpgcheck=1,gpgcheck=0,gi /etc/yum.repos.d/mysql-community.repo
-yum repolist all | grep mysql
-yum install mysql-community-client -y
+yum install --nogpgcheck mysql-community-client -y
--- ch9/9.3/setup.sql
+++ ch9/9.3/setup.sql
@@ -1,3 +1,11 @@
-CREATE DATABASE test CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
-USE test;
-CREATE TABLE session_record (id int AUTO_INCREMENT, trace_id text, access_token text, user_uuid text, PRIMARY KEY (`id`));
+SET @old_autocommit=@@autocommit;
+
+--
+-- Current Database: `world`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `world`*/;
+
+CREATE DATABASE `world` DEFAULT CHARACTER SET utf8mb4;
+
+USE `world`;

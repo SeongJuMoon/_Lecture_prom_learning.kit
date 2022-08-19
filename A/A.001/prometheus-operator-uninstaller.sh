f254043da1385e32570ae42e3278175ab02f1dbe
Hoon Jo <pagaia@hotmail.com>
2022-08-19 17:39:55 +0900

    upt uninstaller

---
+++ A/A.001/prometheus-operator-uninstaller.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch9/9.3
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch9/9.2
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.3/prometheus.yaml
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.2/prometheus-operator.yaml

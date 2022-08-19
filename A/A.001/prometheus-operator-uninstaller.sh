a1b0920596d65d66cdc4dabac748ed1469b48794
Seongju Moon <seongju.moon@linux.com>
2022-08-20 01:12:28 +0900

    update remove operator & dependencies

--- A/A.001/prometheus-operator-uninstaller.sh
+++ A/A.001/prometheus-operator-uninstaller.sh
@@ -2,6 +2,8 @@
 
 kubectl delete -f ~/_Lecture_prom_learning.kit/ch9/9.3
 kubectl delete -f ~/_Lecture_prom_learning.kit/ch9/9.2
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.3/kube-state-metrics/
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.3/node-exporter/
 kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.3/prometheus/
 kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.2/prometheus-operator/
 kubectl delete -f ~/_Lecture_prom_learning.kit/ch8/8.2/crds

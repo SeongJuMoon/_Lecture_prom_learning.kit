8efba8090d1fa7ccf7fc27bdbf76145f14728ff0
Seongju Moon <seongju.moon@linux.com>
2022-08-20 16:17:06 +0900

    added blackbox monitor lab

--- ch7/7.4/.cmd
+++ ch7/7.4/.cmd
@@ -1,3 +1,11 @@
-kubectl apply -f ~/_L 
-kubectl patch cm -n monitoring prometheus-server --patch-file bookinfo-probe-monitoring.yaml
+~/_Lecture_prom_learning.kit/ch7/7.4/install-istio.sh
+istioctl install --set profile=demo -y
 
+kubectl create namespace bookinfo
+kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/sample/bookinfo
+
+kubectl apply ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter.yaml
+kubectl apply ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter-service.yaml
+
+kubectl patch configmap -n monitoring prometheus-server --patch-file bookinfo-probe-monitoring.yaml
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch7/7.4/sample/bookinfo

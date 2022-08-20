2da068b742377e0563bdc33c34565f74e7a7f596
Seongju Moon <seongju.moon@linux.com>
2022-08-20 16:28:27 +0900

    fix wrong path route

--- ch7/7.4/.cmd
+++ ch7/7.4/.cmd
@@ -2,10 +2,10 @@
 istioctl install --set profile=demo -y
 
 kubectl create namespace bookinfo
-kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/sample/bookinfo
+kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/samples/bookinfo
 
 kubectl apply ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter.yaml
 kubectl apply ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter-service.yaml
 
 kubectl patch configmap -n monitoring prometheus-server --patch-file bookinfo-probe-monitoring.yaml
-kubectl delete -f ~/_Lecture_prom_learning.kit/ch7/7.4/sample/bookinfo
+kubectl delete -f ~/_Lecture_prom_learning.kit/ch7/7.4/samples/bookinfo

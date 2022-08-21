78aee4933c7bd08273084d92bb50f1b753aa6d22
Seongju Moon <seongju.moon@linux.com>
2022-08-21 18:18:03 +0900

    upt 7.4

--- ch7/7.4/.cmd
+++ ch7/7.4/.cmd
@@ -9,8 +9,7 @@ kubectl label namespace bookinfo istio-injection=enabled
 kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/samples/bookinfo
 kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/samples/addon
 
-kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter.yaml
-kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter-service.yaml
+kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/blackbox-exporter
 
 kubectl patch configmap -n monitoring prometheus-server --patch-file ~/_Lecture_prom_learning.kit/ch7/7.4/bookinfo-probe-monitoring.yaml
 

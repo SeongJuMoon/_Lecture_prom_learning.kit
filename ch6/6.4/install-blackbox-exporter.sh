00ff1a088de85d57407d8ac21627bdc892a53bd1
Seongju Moon <seongju.moon@linux.com>
2022-08-21 09:15:09 +0900

    harbor template

--- ch6/6.4/install-blackbox-exporter.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -d --network=host --name blackbox_exporter \
-prom/blackbox-exporter:v0.22.0
--- ch6/6.4/setup.sh
+++ ch6/6.4/setup.sh
@@ -16,6 +16,10 @@ systemctl start docker
 
 yum install docker-ce-$1 docker-ce-cli-$1 containerd.io-$1 -y
 
+curl -L https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-linux-x86_64 -o docker-compose
+chmod +x docker-compose
+mv docker-compose /usr/local/bin
+
 # git clone prom code
 git clone https://github.com/SeongJuMoon/_Lecture_prom_learning.kit.git
 mv /home/vagrant/_Lecture_prom_learning.kit $HOME

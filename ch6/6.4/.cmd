e4315211362901e591c15d20e35b44e23623aa55
Seongju Moon <seongju.moon@linux.com>
2022-08-27 16:21:01 +0900

    fix

--- ch6/6.4/.cmd
+++ ch6/6.4/.cmd
@@ -1,8 +1,15 @@
 vagrant up
 
+# 슈퍼 푸티를 통해서 192.168.1.64로 접속합니다.
 ~/_Lecture_prom_learning.kit/ch6/6.4/create-certificate.sh
 ~/_Lecture_prom_learning.kit/ch6/6.4/harbor/install.sh
 
-# 슈퍼 푸티를 통해서 192.168.1.64로 접속합니다.
+# 브라우저에서 https://192.168.1.64 접속합니다.
 
-kubectl patch --patch-file ~/_Lecture_prom_learning.kit/ch6/6.4/target-add-to-prometheus.yml
+cat ~/_Lecture_prom_learning.kit/ch6/6.4/harbor/harbor.yml | nl
+
+# 브라우저에서 http://192.168.1.64:9090/metrics 접속합니다.
+
+cat ~/_Lecture_prom_learning.kit/ch6/6.4/target-add-to-prometheus.yml | nl
+
+kubectl patch configmap prometheus-server -n monitoring --patch-file ~/_Lecture_prom_learning.kit/ch6/6.4/target-add-to-prometheus.yml

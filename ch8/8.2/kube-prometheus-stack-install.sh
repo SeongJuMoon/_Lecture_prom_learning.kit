e2d0d249cf2cc854faf0789301540dc63db4f514
Seongju Moon <seongju.moon@linux.com>
2022-08-28 07:16:18 +0900

    change operator setup be helm

---
+++ ch8/8.2/kube-prometheus-stack-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+helm install prometheus edu/kube-prometheus-stack -n monitoring --create-namespace \
+--set defaultRules.create="false" \
+--set grafana.enabled="false" \
+--set prometheus.storage.volumeClaimTemplate.spec.storageClassName="managed-nfs-storage" \
+--set prometheus.storage.volumeClaimTemplate.spec.accessModes[0]="ReadWriteOnce" \
+--set prometheus.storage.volumeClaimTemplate.spec.resources.requests.storage="8Gi" \
+--set prometheus.service.type="LoadBalancer" \
+--set prometheus.service.port="80"
+-f settings.yaml

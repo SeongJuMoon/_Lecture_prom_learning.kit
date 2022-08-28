81d85984b0303a497882d8e2701ad9653602bf2b
Seongju Moon <seongju.moon@linux.com>
2022-08-28 15:16:51 +0900

    promehteus kube stack install update

--- ch8/8.2/kube-prometheus-stack-install.sh
+++ ch8/8.2/kube-prometheus-stack-install.sh
@@ -3,9 +3,6 @@
 helm install prometheus edu/kube-prometheus-stack -n monitoring --create-namespace \
 --set defaultRules.create="false" \
 --set grafana.enabled="false" \
---set prometheus.storage.volumeClaimTemplate.spec.storageClassName="managed-nfs-storage" \
---set prometheus.storage.volumeClaimTemplate.spec.accessModes[0]="ReadWriteOnce" \
---set prometheus.storage.volumeClaimTemplate.spec.resources.requests.storage="8Gi" \
 --set prometheus.service.type="LoadBalancer" \
---set prometheus.service.port="80"
+--set prometheus.service.port="80" \
 -f ~/_Lecture_prometheus_learning.kit/ch8/8.2/settings.yaml

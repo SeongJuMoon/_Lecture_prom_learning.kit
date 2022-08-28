bbed04aee010bfbe6e0500c257b893eae0bfdade
Seongju Moon <seongju.moon@linux.com>
2022-08-28 15:24:26 +0900

    fix promehteus kube stack install update

--- ch8/8.2/kube-prometheus-stack-install.sh
+++ ch8/8.2/kube-prometheus-stack-install.sh
@@ -5,4 +5,4 @@ helm install prometheus edu/kube-prometheus-stack -n monitoring --create-namespa
 --set grafana.enabled="false" \
 --set prometheus.service.type="LoadBalancer" \
 --set prometheus.service.port="80" \
--f ~/_Lecture_prometheus_learning.kit/ch8/8.2/settings.yaml
+-f ~/_Lecture_prom_learning.kit/ch8/8.2/settings.yaml

a8a9fe7250a749633f194e61f2d459555089fcff
Hoon Jo <pagaia@hotmail.com>
2022-08-30 18:12:35 +0900

    upt .cmd

--- ch4/4.6/.cmd
+++ ch4/4.6/.cmd
@@ -3,7 +3,7 @@ kubectl apply -f nginx-w-exporter
 
 # check application's metrics on web ui
 nginx_up
-nginx_connections_accepted
+nginx_http_requests_total
 
 # delete deployed nginx 
 kubectl delete -f nginx-w-exporter 

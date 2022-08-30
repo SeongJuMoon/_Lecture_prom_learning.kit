8fe3798f5fbc0524cfcf5f50720e10074de6d923
Hoon Jo <pagaia@hotmail.com>
2022-08-30 17:36:09 +0900

    upt .cmd

--- ch4/4.6/.cmd
+++ ch4/4.6/.cmd
@@ -1,6 +1,9 @@
 # apply nginx directory 
-kubectl apply -f nginx
+kubectl apply -f nginx-w-exporter 
 
 # check application's metrics on web ui
 nginx_up
 nginx_connections_accepted
+
+# delete deployed nginx 
+kubectl delete -f nginx-w-exporter 

6cbf07384126dc1ea8954c2ebf28f507e9165d05
Hoon Jo <pagaia@hotmail.com>
2022-08-30 17:32:54 +0900

    upt app-exporter

--- ch4/4.6/.cmd
+++ ch4/4.6/.cmd
@@ -1,1 +1,6 @@
- kubectl apply -f _Lecture_prom_learning.kit/ch4/4.6/nginx/
+# apply nginx directory 
+kubectl apply -f nginx
+
+# check application's metrics on web ui
+nginx_up
+nginx_connections_accepted

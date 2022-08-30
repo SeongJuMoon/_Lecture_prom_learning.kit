84d426965fe6b48b3cd71aff553328ddef00bd8b
Hoon Jo <pagaia@hotmail.com>
2022-08-30 10:09:36 +0900

    add .cmd

---
+++ ch6/6.5/.cmd
@@ -0,0 +1,7 @@
+# level:metric:operations
+
+# add node.rules on Rules menu
+kubectl patch configmap prometheus-server -n monitoring --patch-file node.rules.yaml
+
+# delete node.rules on Rules menu
+kubectl patch configmap prometheus-server -n monitoring --patch-file rules-delete.yaml 

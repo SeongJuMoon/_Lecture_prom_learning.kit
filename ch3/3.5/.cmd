65c45f28c8c82d5661ef89c093435b700a238b4f
Hoon Jo <pagaia@hotmail.com>
2022-08-30 10:07:49 +0900

    rmv no use anymore

--- ch3/3.5/.cmd
+++
@@ -1,7 +0,0 @@
-# level:metric:operations
-
-# add node.rules on Rules menu
-kubectl patch configmap prometheus-server -n monitoring --patch-file node.rules.yaml
-
-# delete node.rules on Rules menu
-kubectl patch configmap prometheus-server -n monitoring --patch-file rules-delete.yaml 

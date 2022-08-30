e64665fa8adf3a660e6ac9942eb545539423fcf4
Hoon Jo <pagaia@hotmail.com>
2022-08-30 16:23:59 +0900

    upt kube-state-metrics

---
+++ ch4/4.4/.cmd
@@ -0,0 +1,16 @@
+# check kube-state-metrics 
+kubectl apply -f deploy-httpd.yaml 
+
+# check replicas's number on web ui 
+kube_deployment_status_replicas_available
+kube_deployment_status_replicas_available{deployment="deploy-httpd"}
+
+# scale out deployment 
+k scale deploy deploy-httpd --replicas 6
+
+# check replicas's number on web ui 
+kube_deployment_status_replicas_available
+kube_deployment_status_replicas_available{deployment="deploy-httpd"}
+
+# check to delete deployment 
+kubectl delete -f deploy-httpd.yaml 

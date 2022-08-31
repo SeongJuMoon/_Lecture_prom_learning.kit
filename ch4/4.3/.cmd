8f8e8f55d0b69e6283f8abb61a93edd1a7f13bc3
Hoon Jo <pagaia@hotmail.com>
2022-08-31 21:34:11 +0900

    upt .cmd

--- ch4/4.3/.cmd
+++ ch4/4.3/.cmd
@@ -1,11 +1,11 @@
-# memory load : approx 50M consumed 
+# memory load on w3-k8s > approx 50M consumed 
 stress --vm 5 --vm-bytes 10M --timeout 150s
 
 # check node memory 
 node_memory_Active_bytes
 node_memory_Active_bytes{node="w3-k8s"}
 
-# cpu load 
+# cpu load on w3-k8s
 stress --cpu 2 --timeout 300s
 
 # check node cpu

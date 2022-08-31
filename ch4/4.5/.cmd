d283e54d0e519d07ea70c67559f68d26bf169ed1
Hoon Jo <pagaia@hotmail.com>
2022-08-31 21:19:38 +0900

    upt metallb_allocated_ip

--- ch4/4.5/.cmd
+++ ch4/4.5/.cmd
@@ -1,16 +1,18 @@
-# 192.168.1.11 프로메테우스 UI로 이동합니다.
+# 1.Check Status and Config
+# check metallb allocated address on web ui 
+metallb_allocator_addresses_in_use_total
 
-metallb_allocator_addresses_in_use_total #2
-
-kubectl create deploy chk-hn --image sysnet4admin/chk-hn --replicas=1
-kubectl expose deploy chk-hn --name chk-hn-loadbalancer --type=LoadBalancer --port 80 --target-port 80
-
-metallb_allocator_addresses_in_use_total #3
-
-# 메탈 LB에 prometheus.io/scrape 및 prometheus.io/port 정보가 설정되있는 것을 확인합니다
+# To check 'prometheus.io/scrape', 'prometheus.io/port' for collecting metrics info from metallb's speak
 kubectl get pods -n metallb-system -o yaml | grep annotation -C 3
 
-# 그러면, 이렇게 설정한 정보는 어떻게 프로메테우스가 가져올까요? 그 답은
-프로메테우스 컨피그 서버에 존재합니다.
+# ConfigMap have target point to collect metrics info 
+kubectl get cm prometheus-server -n monitoring -o yaml | grep -A 50 kubernetes-pods
 
-kubectl get cm prometheus-server -n monitoring -o yaml | nl
+# 2.Deploy new loadbalancer and Check Status 
+# deploy new deploy & svc for increasing lb allocated address
+kubectl apply -f deploy-svc-chk-hn.yaml
+
+# check metallb allocated address on web ui 
+metallb_allocator_addresses_in_use_total
+
+

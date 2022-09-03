713f0b4e6c3060b61dac810ac08f715a35502510
Seongju Moon <seongju.moon@linux.com>
2022-09-03 23:45:44 +0900

    add kube-proxy update shell

--- ch4/4.7/setup-metrics-kube-proxy.sh
+++ ch4/4.7/setup-metrics-kube-proxy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# update config 
 kubectl get configmap -n kube-system kube-proxy -o yaml  | sed -r "s#^(metricsBindAddress: ).*#\10.0.0.0:10249#" | kubectl replace -f -
+# restart
+kubectl rollout restart daemonset n kube-system kube-proxy
+#

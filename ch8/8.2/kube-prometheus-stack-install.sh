6386673ab481990116591278917d4ee43ffe7a98
Seongju Moon <seongju.moon@linux.com>
2022-09-03 22:02:38 +0900

    refactor code for 8.2 labs

--- ch8/8.2/kube-prometheus-stack-install.sh
+++ ch8/8.2/kube-prometheus-stack-install.sh
@@ -5,4 +5,6 @@ helm install prometheus edu/kube-prometheus-stack -n monitoring --create-namespa
 --set grafana.enabled="false" \
 --set prometheus.service.type="LoadBalancer" \
 --set prometheus.service.port="80" \
+--set prometheus.prometheusSpec.scrapeInterval="1m" \
+--set prometheus.prometheusSpec.evaluationInterval="1m" \
 -f ~/_Lecture_prom_learning.kit/ch8/8.2/settings.yaml

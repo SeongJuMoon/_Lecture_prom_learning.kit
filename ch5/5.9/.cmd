b7c04f9ffb0a0421aab8ab19d6b313071841de42
Hoon Jo <pagaia@hotmail.com>
2022-09-03 13:47:06 +0900

    upt complext promql

--- ch5/5.9/.cmd
+++ ch5/5.9/.cmd
@@ -8,5 +8,5 @@ k get po -A | wc -l
 # check deployed apps by namespace 
 count (kube_pod_info) by (namespace)
 
-# check top 3 memory consuming app  
+# check top 3 memory consuming app(no pod name is node's cosuming memory)
 topk(3,sum(container_memory_working_set_bytes{pod!=""}/1024/1024) by (pod,image))

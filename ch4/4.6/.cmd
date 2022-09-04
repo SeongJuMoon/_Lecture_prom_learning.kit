e2cb438cef99250c1583ff829ebd284ff5d05965
sysnet4admin <pagaia@hotmail.com>
2022-09-04 12:38:32 +0900

    swp 4.5 and 4.6

--- ch4/4.6/.cmd
+++ ch4/4.6/.cmd
@@ -1,9 +1,18 @@
-# apply nginx directory 
-kubectl apply -f nginx-w-exporter 
+# 1.Check Status and Config
+# check metallb allocated address on web ui 
+metallb_allocator_addresses_in_use_total
 
-# check application's metrics on web ui
-nginx_up
-nginx_http_requests_total
+# To check 'prometheus.io/scrape', 'prometheus.io/port' for collecting metrics info from metallb's speak
+kubectl get pods -n metallb-system -o yaml | grep annotation -C 3
 
-# delete deployed nginx 
-kubectl delete -f nginx-w-exporter 
+# ConfigMap have target point to collect metrics info 
+kubectl get cm prometheus-server -n monitoring -o yaml | grep -A 50 kubernetes-pods
+
+# 2.Deploy new loadbalancer and Check Status 
+# deploy new deploy & svc for increasing lb allocated address
+kubectl apply -f deploy-svc-chk-hn.yaml
+
+# check metallb allocated address on web ui 
+metallb_allocator_addresses_in_use_total
+
+

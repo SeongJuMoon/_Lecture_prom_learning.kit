de009f3d2affc75cc364774848ad77f5ca0a8963
Hoon Jo <pagaia@hotmail.com>
2022-09-05 13:53:48 +0900

    upt .cmd

--- ch5/5.5/.cmd
+++ ch5/5.5/.cmd
@@ -15,3 +15,7 @@ kubelet_http_requests_total
 sum(kubelet_http_requests_total) without (job)
 sum(kubelet_http_requests_total) without (job,instance,kubernetes_io_arch)
 
+up
+sum(up{job="kubernetes-service-endpoints"}) 
+without(app,app_kubernetes_io_managed_by,chart,component,heritage,k8s_app,kubernetes_io_cluster_service,app_kubernetes_io_component,app_kubernetes_io_instance,app_kubernetes_io_name,app_kubernetes_io_part_of,app_kubernetes_io_version,helm_sh_chart)
+

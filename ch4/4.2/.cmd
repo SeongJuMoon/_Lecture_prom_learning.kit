45c42320b5dac85177120260baaf9ffadad2f31b
Hoon Jo <pagaia@hotmail.com>
2022-09-05 10:37:23 +0900

    upt .cmd

--- ch4/4.2/.cmd
+++ ch4/4.2/.cmd
@@ -1,9 +1,11 @@
 # input this command on prometheus web ui
 container_network_receive_bytes_total
 
-container_network_receive_bytes_total{pod="chk-hn"}
-
+# deployed which is check by prometheus
 k apply -f po-chk-hn.yaml 
+
+# re-input after few seconds 
+container_network_receive_bytes_total{pod="chk-hn"}
 
 curl-get.sh <IP>
 OR
@@ -12,3 +14,5 @@ curl-get-by-name.sh <pod name>
 # check status on prom web ui 
 container_network_receive_bytes_total{pod="chk-hn"}
 
+# delete deployed pod 
+k delete -f po-chk-hn.yaml 

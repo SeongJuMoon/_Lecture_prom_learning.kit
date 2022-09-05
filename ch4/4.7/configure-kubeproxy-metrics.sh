ebc972f6a848ff135e7dabec4849ddda45bbd0e0
Hoon Jo <pagaia@hotmail.com>
2022-09-05 12:13:04 +0900

    chg name and code

--- ch4/4.7/configure-kubeproxy-metrics.sh
+++ ch4/4.7/configure-kubeproxy-metrics.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# update config 
+# update configmap by replace option 
 kubectl get configmap -n kube-system kube-proxy -o yaml  | sed -r "s/(metricsBindAddress: ).*$/\1\"0.0.0.0:10249\"/" | kubectl replace -f -
 
-# restart daemonset for update config
+# restart daemonset for applying modified configmap  
 kubectl rollout restart daemonset -n kube-system kube-proxy
 
-# add scrape config to scrape kube-proxy metrics with replace update
+# add scrape information for collecting kkube-proxy metrics 
 kubectl replace -f kubeproxy_scrape-updater.yaml 

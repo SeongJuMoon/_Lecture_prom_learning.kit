8cea77dcd4a64c7a732aec327791ee9768b4b164
Hoon Jo <pagaia@hotmail.com>
2022-09-06 11:24:35 +0900

    upt replace & patch

--- ch6/6.2/.cmd
+++ ch6/6.2/.cmd
@@ -1,1 +1,6 @@
-kubectl get configmap prometheus-server -o jsonpath="{.data.prometheus\.yml}" -n monitoring 
+# change scrape interval for 1m by replace 
+kubectl replace -f replace-scrape-interval-1m.yaml
+
+# change scrape interval for 15s by patch 
+kubectl patch -n monitoring configmap prometheus-server --patch-file patch-scrape-interval-15s.yaml 
+

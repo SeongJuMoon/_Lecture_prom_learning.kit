d888b94f573f128dd746933a2141de7c00660ddd
Hoon Jo <pagaia@hotmail.com>
2022-09-06 09:33:15 +0900

    upt installer

--- ch8/8.2/prometheus-operator-installer-1m.sh
+++ ch8/8.2/prometheus-operator-installer-1m.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+# create namespace again so that user could start from here 
+# scrape default is 30s 
 
-helm install prometheus edu/kube-prometheus-stack -n monitoring --create-namespace \
+helm install prometheus-operator edu/kube-prometheus-stack  \
 --set defaultRules.create="false" \
 --set grafana.enabled="false" \
 --set prometheus.service.type="LoadBalancer" \
 --set prometheus.service.port="80" \
 --set prometheus.prometheusSpec.scrapeInterval="1m" \
 --set prometheus.prometheusSpec.evaluationInterval="1m" \
--f ~/_Lecture_prom_learning.kit/ch8/8.2/settings.yaml
+--namespace=monitoring \
+--create-namespace \
+-f ~/_Lecture_prom_learning.kit/ch8/8.2/extra-config.yaml 

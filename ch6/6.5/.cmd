385dc9b3c2fb73b4f70a3121eb8b70e14ffe44d8
Hoon Jo <pagaia@hotmail.com>
2022-09-06 17:58:04 +0900

    upt .cmd

--- ch6/6.5/.cmd
+++ ch6/6.5/.cmd
@@ -2,6 +2,8 @@
 
 # add node.rules on Rules menu
 kubectl patch configmap prometheus-server -n monitoring --patch-file node.rules.yaml
+# check rules and PromQL on prometheus web ui 
+node:node_memory:usage
 
 # delete node.rules on Rules menu
 kubectl patch configmap prometheus-server -n monitoring --patch-file init-rules.yaml 

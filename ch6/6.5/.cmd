c9cdd721d7a83069dda97c4c77378350556e6e8b
Hoon Jo <pagaia@hotmail.com>
2022-09-06 18:10:26 +0900

    upt .cmd

--- ch6/6.5/.cmd
+++ ch6/6.5/.cmd
@@ -1,5 +1,7 @@
-~/_Lecture_prom_learning.kit/ch6/6.6/prometheus-alertmanager-installer.sh
+# install prometheus-alertmanager 
+./prometheus-alertmanager-installer.sh
 
+#
 kubectl patch configmap prometheus-alertmanager --patch-file ~/_Lecture_prom_learning.kit/ch6/6.6/server-binding-alertmanager.yaml
 sed s,Slack-URL,<yourslackurl>,gi ~/_Lecture_prom_learning.kit/ch6/6.6/alertmanager-config.yaml
 kubectl patch configmap prometheus-server --patch-file ~/_Lecture_prom_learning.kit/ch6/6.6/alertmanager-config.yaml

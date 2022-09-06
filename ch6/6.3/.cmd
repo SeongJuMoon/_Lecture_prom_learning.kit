01c28cc353c38ef363d50682a9cfebe3660bd0c7
Seongju Moon <seongju.moon@linux.com>
2022-09-07 00:50:03 +0900

    add remove harbor & scrape config

--- ch6/6.3/.cmd
+++ ch6/6.3/.cmd
@@ -20,11 +20,23 @@ http://192.168.1.63:9090/metrics
 # 4.add exposed metrics to the prometheus(SD/service discovery)
 # run ON m-k8s 
 # confirm & add static address app 
-cat add-harbor-to-the-prometheus.yaml | grep harbor -A 5
-kubectl patch configmap prometheus-server -n monitoring --patch-file 4.add-harbor-to-the-prometheus.yaml 
+cat ~/_Lecture_prom_learning.kit/ch6/6.3/4.add-harbor-to-the-prometheus.yaml | grep harbor -A 5
+kubectl patch configmap prometheus-server -n monitoring --patch-file ~/_Lecture_prom_learning.kit/ch6/6.3/4.add-harbor-to-the-prometheus.yaml 
 # check register on prometheus web ui's target 
 
 # 5.validation for metrics value on harbor 
 ./5.docker-image-pullpush-to-harbor.sh
 # check harbor's library status thru web browse (192.168.1.63) i.e. nginx uploaded?
 PromQL: harbor_project_repo_total
+
+# 6. removal harbor scrape config by overwrite prometheus config
+# check there are return empty content.
+cat ~/_Lecture_prom_learning.kit/ch6/6.3/6.removal-harbor-to-the-prometheus.yaml | grep harbor
+# mutate prometheus-server config
+kubectl patch configmap prometheus-server -n monitoring --patch-file ~/_Lecture_prom_learning.kit/ch6/6.3/6.removal-harbor-to-the-prometheus.yaml
+# check register on prometheus web ui's target 
+
+#7 agrant destroy for new harbor VM on HOST machine(windows,MacOS)
+cd ~/_Lecture_prom_learning.kit/ch6/6.3/1.vagrantup-harbor 
+vagrant destroy -f
+# check your virtualbox host ui, there are no harbor vm instance.

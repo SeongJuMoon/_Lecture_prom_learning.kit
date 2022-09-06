f1fe2f00e949812cd92d3c0853cdaec85542cd22
Hoon Jo <pagaia@hotmail.com>
2022-09-06 14:49:28 +0900

    swp dir 6.3 and 6.4

--- ch6/6.4/.cmd
+++ ch6/6.4/.cmd
@@ -1,27 +1,11 @@
-# vagrantup for new harbor VM 
-cd ~/_Lecture_prom_learning.kit/ch6/6.4/create-certificate.sh
-vagrant up
+# check 
+promtool check 
 
-# harbor
-# 슈퍼 푸티를 통해서 192.168.1.64로 접속합니다.
-~/_Lecture_prom_learning.kit/ch6/6.4/create-certificate.sh
-~/_Lecture_prom_learning.kit/ch6/6.4/harbor/install.sh
+# query 
+promtool query 
 
-# 브라우저에서 https://192.168.1.64 접속합니다.
+# debug 
+promtool debug
 
-cat ~/_Lecture_prom_learning.kit/ch6/6.4/harbor/harbor.yml | nl
-
-# 브라우저에서 http://192.168.1.64:9090/metrics 접속합니다.
-
-
-# m-k8s
-cat ~/_Lecture_prom_learning.kit/ch6/6.4/target-add-to-prometheus.yml | nl
-
-kubectl patch configmap prometheus-server -n monitoring --patch-file ~/_Lecture_prom_learning.kit/ch6/6.4/target-add-to-prometheus.yml
-
-docker pull nginx
-docker tag nginx 192.168.1.64/library/nginx
-docker login 192.168.1.64 
-admin
-admin
-docker push 192.168.1.64/library/nginx
+# tsdb 
+promtool tsdb 

c42cedeb68822ba4033acd385f37dfb21694741e
Hoon Jo <pagaia@hotmail.com>
2022-09-07 09:47:25 +0900

    rearrange

--- ch6/6.2/3.abort-kubeproxy-metrics.sh
+++ ch6/6.2/3.abort-kubeproxy-metrics.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  
 # delete annotations for aborting colletion metrics by kubernetes-pod 
-kubectl patch -n monitoring configmap prometheus-server --patch-file kubeproxy-template-annotations/disabled.yaml 
+kubectl patch daemonset -n kube-system kube-proxy --patch-file kubeproxy-template-annotations/disabled.yaml 
 
 echo "successfully abort the metrics and scrape for kubeproxy!"

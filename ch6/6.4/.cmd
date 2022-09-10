1a35eb93889a58601fdea6d767c1e4f7ba8327ff
sysnet4admin <pagaia@hotmail.com>
2022-09-10 18:58:29 +0900

    chg names

--- ch6/6.4/.cmd
+++ ch6/6.4/.cmd
@@ -1,18 +1,14 @@
 # level:metric:operations
 
 # add container rule at Rules menu
-kubectl patch configmap prometheus-server -n monitoring --patch-file prometheus-recording.rules-4-container.yaml
+kubectl patch configmap prometheus-server -n monitoring --patch-file prometheus-recording.rules-container-memory-topk3.yaml
 # check rules and PromQL on prometheus web ui 
 container:memory_working_set:topk3
 
 # add node rule at Rules menu
-kubectl patch configmap prometheus-server -n monitoring --patch-file prometheus-recording.rules-add-node.yaml
+kubectl patch configmap prometheus-server -n monitoring --patch-file prometheus-recording.rules-node-memory-usage.yaml
 # check rules and PromQL on prometheus web ui 
 node:node_memory:usage
 
 # delete all recording rules at Rules menu
 kubectl patch configmap prometheus-server -n monitoring --patch-file init-rules.yaml 
-
-# if you want to init recording rules 
-# run this 
-# kubectl patch configmap prometheus-server -n monitoring --patch-file init-recording.rules.yaml

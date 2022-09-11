d1715e80692d6ac7435de7a82400dd28523b6ebc
Seongju Moon <seongju.moon@linux.com>
2022-09-11 11:34:32 +0900

    update .cmd

--- ch7/7.2/.cmd
+++ ch7/7.2/.cmd
@@ -44,9 +44,15 @@ that means the annotations properties generate target exporter address for redis
 # shortcut click on 192.168.1.11/targets url.
 # find redis-exporter kubernetes-service-endpoints job.
 
-# 11. using browser enter the below query on 192.168.1.11/graph 
+# 10. using browser enter the below query on 192.168.1.11/graph 
 redis_connect_clients
 redis_memory_max_bytes # references on redis-manifest.
+
+# 11. check parameter redis deployment maxmemory.
+kubectl get deployment redis -o yaml | nl
+35  - args:
+36  - --maxmemory 80mb
+37  image: redis:7.0.4
 
 # result
 redis_connect_clients 1
@@ -55,12 +61,12 @@ redis_memory_max_bytes 83886080
 # 12. connect to redis for increase connected_client metrics.
 redis-cli -h 192.168.1.72
 
-# retry enter below query on 192.168.1.11/graph 
+# 13. retry enter below query on 192.168.1.11/graph 
 redis_connect_clients 
 # result 
 redis_connect_clients  2
 
-# 5. generate active memory pressure to redis.
+# 14. generate active memory pressure to redis.
 redis-benchmark -h 192.168.1.72
 PING_INLINE: 11419.68
 
@@ -68,9 +74,9 @@ PING_INLINE: 11419.68
 # You can see that the graph values are drawn to the rightward.
 redis_allocator_active_bytes # <graph>
 
-# release trigger redis_benchmark for next step.
+# 15. release trigger redis_benchmark for next step.
 CTRL+C
 
-# remove redis and redis-exporter
+# 16. remove redis and redis-exporter
 kubectl delete -f 1.redis/
 kubectl delete -f 2.redis-exporter/

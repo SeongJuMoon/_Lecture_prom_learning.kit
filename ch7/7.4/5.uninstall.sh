ce61966ca0a5effc3bebb93eb67b0d732799e648
Seongju Moon <seongju.moon@linux.com>
2022-09-11 10:55:25 +0900

    update 7.4

--- ch7/7.4/5.uninstall.sh
+++ ch7/7.4/5.uninstall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 echo 'remove deployment and namespace'
-kubectl delete -f ./sample/bookinfo
+kubectl delete -f ./samples/bookinfo
+kubectl delete -f ./samples/addon
 kubectl delete namespace bookinfo
 
 echo 'delete istio all component'
-istioctl x uninstall --purge
+istioctl x uninstall --purge -y

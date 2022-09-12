ec90f60c1b1d11ab79b6490c91403d5ed07999db
Seongju Moon <seongju.moon@linux.com>
2022-09-13 00:42:09 +0900

    update .cmd

--- ch7/7.4/.cmd
+++ ch7/7.4/.cmd
@@ -17,6 +17,15 @@ kubectl patch configmap -n monitoring prometheus-server --patch-file 2.add-black
 kubectl scale -n bookinfo deployment details-v1 --replicas=0
 
 # 4. check status on kiali dashbard and query on prometheus web ui
+# check for average on probe duration time by instance. 
+avg(probe_duration_seconds) by (instance)
+
+# 5. check and query on prometheus web ui
+# # check details-v1 service value is 0 that mean details-v1 service got incident.
+sum(probe_success) by (instance) != 1
+
+# check query on prometheus web ui
+# check details-v1 service value is 0
 probe_http_status_code
 
 # 5 delete blackbox configmap & uninstall all of applications 

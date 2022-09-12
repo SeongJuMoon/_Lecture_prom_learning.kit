5be2ec8ef5480cc3fae0a7c7b8cc883d33496f2c
Hoon Jo <pagaia@hotmail.com>
2022-09-12 09:34:49 +0900

    upt cmd

--- ch7/7.2/.cmd
+++ ch7/7.2/.cmd
@@ -1,8 +1,57 @@
-# 0. install redis-cli for benchmark
+# 0.install redis-cli for benchmark
 yum install -y redis
 
-# 1. deploy redis for start lab
-kubectl apply -f 1.redis/
+# 1-1.deply at a once for redis 
+kubectl apply -f 1.redis-w-exporter.yaml
+
+# check to add the target and promql 
+redis_uptime_in_seconds
+redis_memory_max_bytes # 0
+
+# 1-3.delete deployed redis 
+kubectl delete -f 1.redis-w-exporter.yaml 
+
+
+# 2.actual enviroment 
+# 2-1.deploy redis only 
+kubectl apply -f 2-0.redis.yaml
+
+# check deployed redis and svc 
+# there is no redis on prometheus web ui 
+
+# 2-2.deploy redis-exporter to scrape from redis  
+kubectl apply -f 2-1.redis-exporter.yaml
+
+# 2-3.check to add the target and promql 
+redis_memory_max_bytes # 83886080
+
+# 2-4.install redis-cli for benchmark
+yum install -y redis
+
+# 2-5.run awesome querys with benchmark 
+redis-benchmark -h 192.168.1.72
+
+100 * redis_memory_used_bytes / redis_memory_max_bytes
+redis_config_maxclients - redis_connected_clients
+rate(redis_slowlog_length[5m])
+
+# 2-6 delete all deployed 
+kubectl delete -f 2-1.redis-exporter.yaml
+kubectl delete -f 2-0.redis.yaml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # 2. check subdirectory item 
 ls -rlt 1.redis/

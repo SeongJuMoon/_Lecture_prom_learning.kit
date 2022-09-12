ade21964b2fa1a7840bbbf5e944126c8f9c605b5
Seongju Moon <seongju.moon@linux.com>
2022-09-12 21:29:50 +0900

    fix shell script

--- ch7/7.4/0.deploy-MSA-apps-w-istio.sh
+++ ch7/7.4/0.deploy-MSA-apps-w-istio.sh
@@ -2,14 +2,14 @@
 
 # check istioctl is installed.
 if (command -v istioctl > /dev/null); then
-  echo 'istioctl installed already.'
+  echo "istioctl installed already."
 else
-  echo 'install istioctl'
+  echo "install istioctl"
   source ~/_Lecture_prom_learning.kit/ch7/7.4/istio/install.sh
 fi
 
-echo 'begin to install for istio'
-echo '=========================='
+echo "begin to install for istio"
+echo "=========================="
 istioctl install --set profile=demo -y
 
 kubectl create namespace bookinfo
@@ -18,5 +18,5 @@ kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/bookinfo
 kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/addon
 echo "========================================"
 echo "istio's dashboard(kiali) is deploying.."
-echo "kiali grpah URL is http://192.168.1.20:20001/kiali/console/graph/"
+echo "kiali graph URL is http://192.168.1.20:20001/kiali/console/graph/"
 

25cab6dc3122fb0fcd0c817303aaf555be0ba299
Seongju Moon <seongju.moon@linux.com>
2022-09-12 10:08:20 +0900

    update mysql

--- ch7/7.3/.cmd
+++ ch7/7.3/.cmd
@@ -1,19 +1,29 @@
-# 0. check list directory files.
-ls -rlt
-# 1.install-mysql-client.sh  2.mysql  db_scripts
+# 1-1.deploy at a once for mysql 
+kubectl apply -f 1.mysql-w-exporter.yaml
 
-# 1. install denpencies mysql-client 8.0 for connect to mysql pod.
-./1.install-mysql-client.sh
+# check to add the target and promql 
+# mysql uptime.
+mysql_global_status_uptime
+# get db available connect percent.
+100 * mysql_global_status_threads_connected / mysql_global_variables_max_connections
 
-# 2. check manifest mysql and mysql exporter.
-ls -rlt 2.mysql
-# service.yaml
-# mysql.yaml
+# 1-2.delete deployed mysql 
+kubectl delete -f 1.mysql-w-exporter.yaml 
 
-# 3. deploy mysql and mysql exporter.
+# 2.actual enviroment 
+# 2-1.deploy mysql only 
+kubectl apply -f 2-0.mysql.yaml
+
+# check deployed mysql and svc 
+# there is no mysql on prometheus web ui 
+
+# 2-2.deploy mysqld-exporter to scrape from mysql  
+kubectl apply -f 2-1.mysqld-exporter.yaml
+
+# 3. deploy mysql and mysqld exporter.
 kubectl apply -f 2.mysql/
 
-# 4. check mysql and mysql exporter.
+# 4. check mysql and mysqld exporter.
 kubectl get service,deployment mysql
 
 # result
@@ -23,21 +33,23 @@ service/mysql   LoadBalancer   10.99.154.164   192.168.1.73   3306:30935/TCP,910
 NAME                    READY   UP-TO-DATE   AVAILABLE   AGE
 deployment.apps/mysql   1/1     1            1           11m
 
+# 5. install mysql client tool.
+./3.install-mysql-client.sh
 
-# 5. check mysql service annotations
+# 6. check mysql service annotations
 kubectl get services mysql -o yaml | nl
 7  prometheus.io/port: "9104"
 8  prometheus.io/scrape: "true"
 
-# 6. check target ui on prometheus ui(http://192.168.1.11/targets)
+# 7. check target ui on prometheus ui(http://192.168.1.11/targets)
 search CTRL + F enter mysql
 
-# 7. prometheus ui(http://192.168.1.11/graph) below query.
+# 8. prometheus ui(http://192.168.1.11/graph) below query.
 mysql_global_variables_max_allowed_packet
 # result
 mysql_global_variables_max_allowed_packet 67108864
 
-# check max_allowed_packet in mysql.
+# 9. check max_allowed_packet in mysql.
 # connect to mysql pod
 mysql -h 192.168.1.73 -u root -p
 # password is 'root'
@@ -45,10 +57,10 @@ mysql -h 192.168.1.73 -u root -p
 show variables like 'max_allowed_packet';
 max_allowed_packet | 67108864 
 
-# 8. add database
+# 10. add database
 source ./db_scripts/1.setup.sql;
 
-# 9. check `world` database added.
+# 11. check `world` database added.
 show databases;
 
 #result
@@ -56,29 +68,29 @@ show databases;
 world
 ...
 
-# 10. change mysql variables `max_allowed_packet` to 1024. 
+# 12. change mysql variables `max_allowed_packet` to 1024. 
 set global max_allowed_packet=1024;
 
-# 11. check browser on prometheus ui(http://192.168.1.11/graph) enter the below query.
+# 13. check browser on prometheus ui(http://192.168.1.11/graph) enter the below query.
 mysql_global_variables_max_allowed_packet 
 # result mysql_global_variables_max_allowed_packet 1024
 
-# 12. check `max_allowed_packet` variables at the active client to variables changed.
+# 14. check `max_allowed_packet` variables at the active client to variables changed.
 show variables like 'max_allowed_packet';
 max_allowed_packet | 67108864 
 
-# 13. reconnect mysql to max_allowed_packet variable has been updated. 
+# 15. reconnect mysql to max_allowed_packet variable has been updated. 
 CTRL + d
 # result
 mysql> ^DBye
 mysql -h 192.168.1.73 -u root -p
 # password is 'root'
 
-# 14. check retry max_allowed_packet variables.
+# 16. check retry max_allowed_packet variables.
 show variables like 'max_allowed_packet';
 max_allowed_packet | 1024 
 
-# 15. running data migration below command in mysql client.
+# 17. running data migration below command in mysql client.
 source db_scripts/2.insert.sql
 ...
 Reading table information for completion of table and column names
@@ -104,17 +116,17 @@ mysql> ^DBye
 mysql -h 192.168.1.73 -u root -p
 # password is 'root'
 
-# 16. check max_allowed_packet variables is updated to 67108864.
+# 18. check max_allowed_packet variables is updated to 67108864.
 show variables like 'max_allowed_packet';
 # result
 max_allowed_packet | 67108864
 
-# 17. check max_allowed_packet variables at prometheus web ui(192.168.1.73) using below query.
+# 19. check max_allowed_packet variables at prometheus web ui(192.168.1.73) using below query.
 mysql_global_variables_max_allowed_packet
 # result 
 mysql_global_variables_max_allowed_packet 67108864
 
-# 18. retry insert on mysql pod.
+# 20. retry insert on mysql pod.
 source db_scripts/2.insert.sql
 # result 
 ...
@@ -122,14 +134,15 @@ Query OK, 4079 rows affected (1.19 sec)
 Records: 4079  Duplicates: 0  Warnings: 0
 ...
 
-# 19. check data is inserted on table is below query.
+# 21. check data is inserted on table is below query.
 select count(1) from world.city;
 # result
 4079
 
-# 20. disconnect mysql to prepare next lecture.
+# 22. disconnect mysql to prepare next lecture.
 CTRL + D
 mysql> ^DBye
 
-# 21. cleanup resource to next lecture.
-kubectl delete -f ./2.mysql
+# 23. cleanup resource to next lecture.
+kubectl delete -f ./2-1.mysqld-exporter.yaml
+kubectl delete -f ./2-0.mysql.yaml

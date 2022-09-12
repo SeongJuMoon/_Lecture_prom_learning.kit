469bc042f2c29addf2bbc8c0e69f665e177d5bf8
Hoon Jo <pagaia@hotmail.com>
2022-09-12 15:51:44 +0900

    upt rearrange all

---
+++ ch7/7.4/0.deploy-MSA-apps-w-istio.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# check istioctl is installed.
+if (command -v istioctl > /dev/null); then
+  echo 'istioctl installed already.'
+else
+  echo 'install istioctl'
+  source ~/_Lecture_prom_learning.kit/ch7/7.4/istio/install.sh
+fi
+
+echo 'begin to install for istio'
+echo '=========================='
+istioctl install --set profile=demo -y
+
+kubectl create namespace bookinfo
+kubectl label namespace bookinfo istio-injection=enabled
+kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/bookinfo
+kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/addon
+echo '========================================'
+echo 'istio's dashboard(kiali) is deploying...'
+echo 'kiali grpah URL is http://192.168.1.20:20001/kiali/console/graph/'
+

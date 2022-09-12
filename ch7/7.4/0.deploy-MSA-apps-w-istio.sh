77f10365304dce5749e2bd0c61b0dcd36445a6fe
Seongju Moon <seongju.moon@linux.com>
2022-09-12 21:23:49 +0900

    escape ( )

--- ch7/7.4/0.deploy-MSA-apps-w-istio.sh
+++ ch7/7.4/0.deploy-MSA-apps-w-istio.sh
@@ -16,7 +16,7 @@ kubectl create namespace bookinfo
 kubectl label namespace bookinfo istio-injection=enabled
 kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/bookinfo
 kubectl apply -f ~/_Lecture_prom_learning.kit/ch7/7.4/istio/samples/addon
-echo '========================================'
-echo 'istio's dashboard(kiali) is deploying...'
-echo 'kiali grpah URL is http://192.168.1.20:20001/kiali/console/graph/'
+echo "========================================"
+echo "istio's dashboard(kiali) is deploying.."
+echo "kiali grpah URL is http://192.168.1.20:20001/kiali/console/graph/"
 

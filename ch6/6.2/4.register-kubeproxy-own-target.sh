856eb7b7d4c090b174241d5ad87c7f6d540b4377
sysnet4admin <pagaia@hotmail.com>
2022-09-12 22:48:52 +0900

    res addr

--- ch6/6.2/4.register-kubeproxy-own-target.sh
+++ ch6/6.2/4.register-kubeproxy-own-target.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # register new kube-proxy job for prometheus  
-kubectl patch -n monitoring configmap prometheus-server --patch-file ~/_Lecture_prom_learning.kit/ch6/6.2/promethues-kubeproxy-job/enabled-15s.yaml 
+kubectl patch -n monitoring configmap prometheus-server --patch-file promethues-kubeproxy-job/enabled-15s.yaml 
 
 echo "successfully register new job for kubeproxy!"

fd64af58977514378b3ba22e594daddc424e7069
Seongju Moon <seongju.moon@linux.com>
2022-09-13 22:09:42 +0900

    add script file

---
+++ ch7/7.4/istio/load-generator-10s.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Exit Process Enter [CTRL + C]"
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+while true; do curl -s -o /dev/null "http://$INGRESS_HOST/productpage";sleep 10; done

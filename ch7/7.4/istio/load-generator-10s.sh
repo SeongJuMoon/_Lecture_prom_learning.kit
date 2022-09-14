b5564690fb7b375b38f36aebfdad23cfa8a4971c
Hoon Jo <pagaia@hotmail.com>
2022-09-14 14:49:01 +0900

    add comment for 10sec

--- ch7/7.4/istio/load-generator-10s.sh
+++ ch7/7.4/istio/load-generator-10s.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# kiali draw graph per 15sec as default
+# thus 10sec is reasonable choice 
 
 echo "Exit Process Enter [CTRL + C]"
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')

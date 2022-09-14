46e349ddca38f42a64eb206908c8643285b15ca6
Hoon Jo <pagaia@hotmail.com>
2022-09-14 14:59:40 +0900

    upt .cmd

--- ch8/8.2/.cmd
+++ ch8/8.2/.cmd
@@ -1,2 +1,5 @@
-# 쿠버네티스 오퍼레이터 및 랩 환경을 구축을 위해 실행합니다.
-~/_Lecture_prom_learning.kit/ch8/8.2/kube-prometheus-stack-install.sh
+# install kube-prometheus-stack
+# it operate by prometheus operator
+
+~/_Lecture_prom_learning.kit/ch8/8.2/kube-prometheus-stack-installer-15s.sh
+

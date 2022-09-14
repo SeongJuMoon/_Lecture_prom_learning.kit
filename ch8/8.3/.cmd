4077e14580a4700931055d25069e5011ca7c4862
Hoon Jo <pagaia@hotmail.com>
2022-09-14 15:35:08 +0900

    rearrange

--- ch8/8.3/.cmd
+++ ch8/8.3/.cmd
@@ -1,4 +1,5 @@
-# 1. 서비스 모니터를 확인하기 위해서 수집 대상 애플리케이션인 엔진엑스를 배포합니다.
-kubectl apply -f ~/_Lecture_prom_learning.kit/ch8/8.7/nginx
+# deploy nginx with exporter to collect metrics from nginx to prometheus operator 
+kubectl apply -f nginx-w-exporter 
 
-# 2. nginx가 배포되었다면, nginx를 오퍼레이터가 관리하는 프로메테우스가 수집할 수 있도록 서비스 모니터를 설정합니다.
+# configure ServiceMonitor which is the operator of prometheus
+kubectl apply -f ServiceMonitor.yaml 

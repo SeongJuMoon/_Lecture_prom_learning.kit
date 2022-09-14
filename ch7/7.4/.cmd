46d3c96149352173f9518fcd08f0706dd81fcbbe
Hoon Jo <pagaia@hotmail.com>
2022-09-14 14:22:29 +0900

    upt .cmd

--- ch7/7.4/.cmd
+++ ch7/7.4/.cmd
@@ -10,16 +10,16 @@ kubectl patch configmap -n monitoring prometheus-server --patch-file 2.add-black
 
 # check blackbox target which is registed 
 
-# 2-2.make a sudden issue that is 'replicas'
-kubectl scale -n bookinfo deployment details-v1 --replicas=0
-
 # check status on kiali dashbard and query on prometheus web ui
 # check for average on probe duration time by instance. 
 avg(probe_duration_seconds) by (instance)
 
+# 2-2.make a sudden issue that is 'replicas'
+kubectl scale -n bookinfo deployment details-v1 --replicas=0
+
 # check and query on prometheus web ui
 # check details-v1 service value is 0 that mean details-v1 service got incident.
-sum(probe_success) by (instance) != 1
+sum(probe_success) by (kubernetes_service_name) != 1
 
 # check query on prometheus web ui
 # check details-v1 service value is 0

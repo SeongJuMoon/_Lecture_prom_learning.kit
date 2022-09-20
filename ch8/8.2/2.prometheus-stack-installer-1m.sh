e3c8c4db0e17415d4038ab1c792310b0fd433fad
Seongju Moon <seongju.moon@linux.com>
2022-09-21 07:51:50 +0900

    remove comments.

--- ch8/8.2/2.prometheus-stack-installer-1m.sh
+++ ch8/8.2/2.prometheus-stack-installer-1m.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# create namespace again so that user could start from here 
-# scrape default is 30s 
 
 helm install prometheus-stack edu/kube-prometheus-stack  \
 --set defaultRules.create="false" \

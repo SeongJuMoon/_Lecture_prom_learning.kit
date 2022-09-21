d793480685d8c5f24ef94800aa814b92d222a207
Hoon Jo <pagaia@hotmail.com>
2022-09-21 16:51:42 +0900

    rearrange

---
+++ ch8/8.8/1.MUST-Run-to-change-Slack-URL.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sed s,Slack-URL,$1,g -i ~/_Lecture_prom_learning/ch8/8.8/+alertmanager.yaml
--- ch8/8.8/1.replace-slack-url.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sed s,Slack-URL,$1,g -i ~/_Lecture_prom_learning/ch8/8.8/extra-config.yaml

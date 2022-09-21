b1d40bc96e74c5c6fe9662fb87332a1debfe7509
Hoon Jo <pagaia@hotmail.com>
2022-09-21 16:45:35 +0900

    rearrange

--- ch8/8.8/prometheus-stack-upgrader-15s.sh
+++ ch8/8.8/prometheus-stack-upgrader-15s.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-helm upgrade -i prometheus edu/kube-prometheus-stack -n monitoring --create-namespace \
+helm upgrade prometheus-stack edu/kube-prometheus-stack \
 --set defaultRules.create="false" \
 --set grafana.enabled="false" \
 --set prometheus.service.type="LoadBalancer" \
 --set prometheus.service.port="80" \
 --set prometheus.prometheusSpec.scrapeInterval="15s" \
 --set prometheus.prometheusSpec.evaluationInterval="15s" \
--f ~/_Lecture_prom_learning.kit/ch8/8.8/extra-config.yaml
+--namespace=monitoring \
+-f ~/_Lecture_prom_learning.kit/ch8/8.8/+alertmanager.yaml
---
+++ ch8/8.8/prometheus-stack-upgrader-30s-default.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# scrape default is 30s for kube-prometheus-stack
+
+helm upgrade prometheus-stack edu/kube-prometheus-stack \
+--set defaultRules.create="false" \
+--set grafana.enabled="false" \
+--set prometheus.service.type="LoadBalancer" \
+--set prometheus.service.port="80" \
+--namespace=monitoring \
+-f ~/_Lecture_prom_learning.kit/ch8/8.8/+alertmanager.yaml

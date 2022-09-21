a8553986f4fa7984473b91cbbaa9a9399b40cd49
Hoon Jo <pagaia@hotmail.com>
2022-09-21 13:48:49 +0900

    upt kube-etcd

--- ch8/8.3/2.prometheus-stack-upgrader-30s-default.sh
+++ ch8/8.3/2.prometheus-stack-upgrader-30s-default.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+# scrape default is 30s for kube-prometheus-stack
 
 helm upgrade prometheus-stack edu/kube-prometheus-stack  \
 --set defaultRules.create="false" \
+--set alertmanager.enabled="false" \
 --set grafana.enabled="false" \
 --set prometheus.service.type="LoadBalancer" \
 --set prometheus.service.port="80" \
---set prometheus.prometheusSpec.scrapeInterval="1m" \
---set prometheus.prometheusSpec.evaluationInterval="1m" \
 --namespace=monitoring \
--f ~/_Lecture_prom_learning.kit/ch8/8.4/extra-config.yaml 
+-f ~/_Lecture_prom_learning.kit/ch8/8.3/+kube-etcd.yaml 

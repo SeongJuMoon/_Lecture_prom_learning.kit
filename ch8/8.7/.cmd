20fdee48411218b167020380f8b65a6b19ef2f82
Hoon Jo <pagaia@hotmail.com>
2022-09-22 17:55:49 +0900

    chg names

--- ch8/8.7/.cmd
+++ ch8/8.7/.cmd
@@ -1,7 +1,9 @@
-프로메테우스 룰을 디플로이한다.
+# apply CRD to create rules 
+kubectl apply -f 1.PrometheusRule-node-memory-usage.yaml
 
-# result 
-prometheusrule.monitoring.coreos.com/container-memory-topk3 created
+# delete rules so that move next step
+kubectl delete -f 1.PrometheusRule-node-memory-usage.yaml
 
-# topk(3,sum(container_memory_working_set_bytes{pod!=""}/1024/1024) by (pod))
-# avg(rate(node_cpu_seconds_total[2m])) by (instance, mode)
+# create rules by operator's configuration
+./2.prometheus-stack-upgrader-15s.sh
+

835c9f5c22c8b722cd1fbf3c18dab569d0b947a7
Hoon Jo <pagaia@hotmail.com>
2022-09-22 21:59:57 +0900

    rearrange all

--- ch9/9.3/.cmd
+++ ch9/9.3/.cmd
@@ -1,135 +1,164 @@
-# 0. check list directory files.
-ls -rlt
-# 1.install-mysql-client.sh  2.mysql  db_scripts
+# 1. prepare istio and sample application.
+./prepare.sh
 
-# 1. install denpencies mysql-client 8.0 for connect to mysql pod.
-./1.install-mysql-client.sh
+# 2. check istio-ingressgateway is active.
+kubectl get svc istio-ingressgateway -n istio-system
 
-# 2. check manifest mysql and mysql exporter.
-ls -rlt 2.mysql
-# service.yaml
-# mysql.yaml
+# 3. check kiali dashboard to check application status is active.
+# http://192.168.1.20:20001/kiali/console/applications
 
-# 3. deploy mysql and mysql exporter.
-kubectl apply -f 2.mysql/
+# 4. check kiali dashboard to see inter application related diagram
+# http://192.168.1.20:20001/kiali/console/graph/
 
-# 4. check mysql and mysql exporter.
-kubectl get service,deployment mysql
+# 4. call load-generator.sh to view service related diagram.
+./load-generator.sh
 
+# cancel load-generator.sh for execute after task.
+CTRL+C
+
+# 4. check blackbox-exporter directory items.
+ls -rlt 2.blackbox-exporter/
+service.yaml deployment.yaml configmap.yaml
+
+
+# 5. deploy blackbox-exporter
+kubectl apply -f ./2.blackbox-exporter
 # result
-NAME            TYPE           CLUSTER-IP      EXTERNAL-IP    PORT(S)                         AGE
-service/mysql   LoadBalancer   10.99.154.164   192.168.1.73   3306:30935/TCP,9104:30828/TCP   11m
+configmap/prometheus-blackbox-exporter created
+deployment.apps/prometheus-blackbox-exporter created
+service/prometheus-blackbox-exporter created
 
-NAME                    READY   UP-TO-DATE   AVAILABLE   AGE
-deployment.apps/mysql   1/1     1            1           11m
+# check blackbox-exporter-config.yaml
+# new job blackbox-kubernetes-healthcheck-services appends
 
+cat 3.add-blackbox-exporter-config.yaml | nl
+     1  data:
+     2    prometheus.yml: |
+     3      global:
+     4        evaluation_interval: 1m
+     5        scrape_interval: 1m
+     6        scrape_timeout: 15s
+     7      rule_files:
+     8      - /etc/config/recording_rules.yml
+     9      - /etc/config/alerting_rules.yml
+    10      - /etc/config/rules
+    11      - /etc/config/alerts
+    12      scrape_configs:
+    13      - job_name: "blackbox-kubernetes-healthcheck-services"
+    14        metrics_path: /probe
+    15        params:
+    16          module: [http_2xx]
+    17          target:
+    18           - health
+    19        kubernetes_sd_configs:
+    20        - role: service
+    21        relabel_configs:
+    22          - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_should_be_probed]
+    23            action: keep
+    24            regex: true
+    25          - source_labels:
+    26            - __address__
+    27            - __meta_kubernetes_service_annotation_prometheus_io_healthcheck_path
+    28            regex: (.*);(.*)
+    29            replacement: http://$1$2
+    30            target_label: __param_target
+    31          - target_label: __address__
+    32            replacement:  prometheus-blackbox-exporter.monitoring.svc.cluster.local:9115
+    33          - source_labels: [__param_target]
+    34            target_label: instance
+    35          - action: labelmap
+    36            regex: __meta_kubernetes_service_label_(.+)
+    37          - source_labels: [__meta_kubernetes_namespace]
+    38            target_label: kubernetes_namespace
+    39          - source_labels: [__meta_kubernetes_service_name]
+    40            target_label: kubernetes_service_name
+    +++
 
-# 5. check mysql service annotations
-kubectl get services mysql -o yaml | nl
-7  prometheus.io/port: "9104"
-8  prometheus.io/scrape: "true"
-
-# 6. check target ui on prometheus ui(http://192.168.1.11/targets)
-search CTRL + F enter mysql
-
-# 7. prometheus ui(http://192.168.1.11/graph) below query.
-mysql_global_variables_max_allowed_packet
+# check target service annotation.
+kubectl get service -n bookinfo
 # result
-mysql_global_variables_max_allowed_packet 67108864
+details       ClusterIP   10.101.1.84      <none>        9080/TCP   50m
+productpage   ClusterIP   10.102.55.5      <none>        9080/TCP   50m
+ratings       ClusterIP   10.108.154.181   <none>        9080/TCP   50m
+reviews       ClusterIP   10.106.153.64    <none>        9080/TCP   50m
 
-# check max_allowed_packet in mysql.
-# connect to mysql pod
-mysql -h 192.168.1.73 -u root -p
-# password is 'root'
-
-show variables like 'max_allowed_packet';
-max_allowed_packet | 67108864 
-
-# 8. add database
-source ./db_scripts/1.setup.sql;
-
-# 9. check `world` database added.
-show databases;
-
-#result
-...
-world
-...
-
-# 10. change mysql variables `max_allowed_packet` to 1024. 
-set global max_allowed_packet=1024;
-
-# 11. check browser on prometheus ui(http://192.168.1.11/graph) enter the below query.
-mysql_global_variables_max_allowed_packet 
-# result mysql_global_variables_max_allowed_packet 1024
-
-# 12. check `max_allowed_packet` variables at the active client to variables changed.
-show variables like 'max_allowed_packet';
-max_allowed_packet | 67108864 
-
-# 13. reconnect mysql to max_allowed_packet variable has been updated. 
-CTRL + d
+kubectl get service -n bookinfo details -o yaml | nl
 # result
-mysql> ^DBye
-mysql -h 192.168.1.73 -u root -p
-# password is 'root'
+7 prometheus.io/healthcheck_path: /health
+8 prometheus.io/should_be_probed: "true"
 
-# 14. check retry max_allowed_packet variables.
-show variables like 'max_allowed_packet';
-max_allowed_packet | 1024 
-
-# 15. running data migration below command in mysql client.
-source db_scripts/2.insert.sql
-...
-Reading table information for completion of table and column names
-You can turn off this feature to get a quicker startup with -A
-
-Database changed
-Query OK, 0 rows affected (0.00 sec)
-
-ERROR 1153 (08S01): Got a packet bigger than 'max_allowed_packet' bytes
-ERROR 2013 (HY000): Lost connection to MySQL server during query
-No connection. Trying to reconnect...
-Connection id:    94
-Current database: world
-
-Query OK, 0 rows affected (0.02 sec)
-
-# initialize max_allowed_packet variables 
-# and reconnect mysql client to apply variables value updated.
-set global max_allowed_packet=64 * 1024 * 1024;
-CTRL + d
+kubectl get service -n bookinfo productpage -o yaml | nl
 # result
-mysql> ^DBye
-mysql -h 192.168.1.73 -u root -p
-# password is 'root'
+7 prometheus.io/healthcheck_path: /health
+8 prometheus.io/should_be_probed: "true"
 
-# 16. check max_allowed_packet variables is updated to 67108864.
-show variables like 'max_allowed_packet';
+kubectl get service -n bookinfo ratings -o yaml | nl
 # result
-max_allowed_packet | 67108864
+7 prometheus.io/healthcheck_path: /health
+8 prometheus.io/should_be_probed: "true"
 
-# 17. check max_allowed_packet variables at prometheus web ui(192.168.1.73) using below query.
-mysql_global_variables_max_allowed_packet
+kubectl get service -n bookinfo reviews -o yaml | nl
+# result
+7 prometheus.io/healthcheck_path: /health
+8 prometheus.io/should_be_probed: "true"
+
+# patch prometheus config to add job blackbox-kubernetes-healthcheck-service.
+kubectl patch configmap -n monitoring prometheus-server --patch-file ./3.add-blackbox-exporter-config.yaml
+
+# check prometheus ui (http://192.168.1.11/targets)  blackbox-kubernetes-healthcheck-service target target count
+# you seen 4 item below 'blackbox-kubernetes-healthcheck-service target' job.
+
+
+# scale down deployment detail-v1 to test probe is possiable.
+kubectl scale -n bookinfo deployment details-v1 --replicas=0
+
+# call load-generator.sh to view graph diagram.
+././load-generator.sh
+
+# check kiali dashboard to see inter application related diagram
+# you seen the detail diagram border red color
+# http://192.168.1.20:20001/kiali/console/graph
+
+# check prometheus web ui(http://192.168.1.11/graph) enter query below.
+# you seen details service status code is not measured.
+
+probe_http_status_code
 # result 
-mysql_global_variables_max_allowed_packet 67108864
 
-# 18. retry insert on mysql pod.
-source db_scripts/2.insert.sql
+probe_http_status_code{app="details", instance="http://details.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="details", service="details"}
+0
+probe_http_status_code{app="productpage", instance="http://productpage.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="productpage", service="productpage"}
+200
+probe_http_status_code{app="ratings", instance="http://ratings.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="ratings", service="ratings"}
+200
+probe_http_status_code{app="reviews", instance="http://reviews.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="reviews", service="reviews"}
+200
+
+# cancel load_generator.sh for next steps.
+CTRL + C
+
+# restore deployment scale for correct metrics.
+kubectl scale -n bookinfo deployment details-v1 --replicas=1
+
+# call load-generator.sh to view graph diagram.
+# checkout http://192.168.1.20:20001/kiali/console/graph/namespaces
+./load-generator.sh
+
+# check prometheus web ui(http://192.168.1.11/graph) enter query below.
+# you seen all service http_status_code return 200.
+
+probe_http_status_code
 # result 
-...
-Query OK, 4079 rows affected (1.19 sec)
-Records: 4079  Duplicates: 0  Warnings: 0
-...
 
-# 19. check data is inserted on table is below query.
-select count(1) from world.city;
-# result
-4079
+probe_http_status_code{app="details", instance="htt://details.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="details", service="details"}
+200
+probe_http_status_code{app="productpage", instance="http://productpage.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="productpage", service="productpage"}
+200
+probe_http_status_code{app="ratings", instance="http://ratings.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="ratings", service="ratings"}
+200
+probe_http_status_code{app="reviews", instance="http://reviews.bookinfo.svc:9080/health", job="blackbox-kubernetes-healthcheck-services", kubernetes_namespace="bookinfo", kubernetes_service_name="reviews", service="reviews"}
+200
 
-# 20. disconnect mysql to prepare next lecture.
-CTRL + D
-mysql> ^DBye
-
-# 21. cleanup resource to next lecture.
-kubectl delete -f ./2.mysql
+# delete 
+./5.uninstall.sh
+kubectl patch -n monitoring configmap prometheus-server --patch-file ./4.delete-blackbox-exporter-config.yaml
---
+++ ch9/README.md
@@ -0,0 +1,6 @@
+### 다루는 내용 
+- 9.2: harbor 
+- 9.3: blackbox 
+- 9.4: rules (show all and restore version)
+- 9.5: alertmanager 
+- 9.6: If it is production? level?, explain real world 

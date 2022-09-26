47ba517fab0ffe02905d469ae0671d865abbceeb
Hoon Jo <pagaia@hotmail.com>
2022-09-26 13:55:13 +0900

    add sleep 5

--- A/A.001/prometheus-force-installer.sh
+++ A/A.001/prometheus-force-installer.sh
@@ -3,6 +3,7 @@
 helm uninstall -n monitoring prometheus > /dev/null 2>&1
 helm uninstall -n monitoring prometheus-stack > /dev/null 2>&1
 kubectl -n monitoring delete pvc prometheus-prometheus-stack-kube-prom-prometheus-db-prometheus-prometheus-stack-kube-prom-prometheus-0 > /dev/null 2>&1
+echo "waiting for deleting prometheus componentes in 5seconds"; sleep 5
 echo "Delete all prometheus components successfully!"
 echo "----------------------------------------------"
 

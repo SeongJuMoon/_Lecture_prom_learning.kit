5d067a6d87dd44a72ee95753ef6c166ade036a9e
Hoon Jo <pagaia@hotmail.com>
2022-09-26 16:02:26 +0900

    upt .cmd

--- ch8/8.5/.cmd
+++ ch8/8.5/.cmd
@@ -1,46 +1,47 @@
-# 0. install redis-cli for behckmark and connect to redis.
+# 0. install redis-cli for behckmark and checking connection list 
 ./0.redis-cli-installer.sh
 
-# 1. deploy redis
+# 1.deploy redis
 kubectl apply -f 1.redis.yaml
 
-# 2. check redis deployed resource
+# 2.check to deploy redis resource
  kubectl get service,deployment -l app=redis
 
-# 3. deploy redis-exporter
+# 3.deploy redis-exporter
 kubectl apply -f 2.redis-exporter.yaml 
 
-# 4. check redis-exporter deployed resource.
+# 4.check to deploy redis-exporter resource
 kubectl get service,deployment -l app=redis-exporter
 
-# 5. configure servicemonitor to prometheus scrape redis metrics.
+# 5.configure ServiceMonitor to scrape prometheus's redis metrics
 kubectl apply -f 3.servicemonitor-redis.yaml
 
-# 6. using browser enter the below query on 192.168.1.11/graph 
+# 6.check this promql on http://192.168.1.11/graph 
 redis_connect_clients
 redis_memory_max_bytes # references on redis-manifest.
 
-# 7. check on browser 192.168.1.11 on target menu.
-# shortcut click on 192.168.1.11/targets url.
-# search redis servicemonitor target 
+# 7.check the targets on prometheus web UI 
+# shortcut URL is 192.168.1.11/targets
+# search & check the redis in the one of serviceMonitor (anyway *new*)
 
-# 8. check parameter redis deployment maxmemory.
+# 8.check maxmemory parameter in redis deployment 
 kubectl get deployment redis -o yaml | nl
 35  - args:
 36  - --maxmemory 80mb
 37  image: redis:7.0.4
 
-# result
+# check redis parameter 
 redis_connect_clients 1
 redis_memory_max_bytes 83886080
 
-# 9. generate active memory pressure to redis.
+# 9.generate query load for increating active memory for checking redis performance
 redis-benchmark -h 192.168.1.85 -l
 PING_INLINE: 11419.68
 
-# 10. enter below query on 192.168.1.11/graph after change graph tab.
-# You can see that the graph values are drawn to the rightward.
+# 10.check promql on 192.168.1.11/graph 
+# You can see that the graph values are drawn to the rightward. ???
 redis_allocator_active_bytes # <graph>
 
+# clear all resources for next lecture 
 kubectl delete -f 1.redis/
 kubectl delete -f 2.redis-exporter/

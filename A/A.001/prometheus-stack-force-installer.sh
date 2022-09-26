ae5a4655a004cc748d80a673b8b64128dc9d54c9
Hoon Jo <pagaia@hotmail.com>
2022-09-26 10:36:40 +0900

    upt force options

--- A/A.001/prometheus-stack-force-installer.sh
+++ A/A.001/prometheus-stack-force-installer.sh
@@ -4,6 +4,7 @@ helm uninstall -n monitoring prometheus > /dev/null 2>&1
 helm uninstall -n monitoring prometheus-stack > /dev/null 2>&1
 kubectl -n monitoring delete pvc prometheus-prometheus-stack-kube-prom-prometheus-db-prometheus-prometheus-stack-kube-prom-prometheus-0 > /dev/null 2>&1
 echo "Delete all prometheus components successfully!"
+echo "----------------------------------------------"
 
 echo "Start to deploy prometheus operator with scraping 15s interval"
 echo "=============================================================="

a11f5e7d76040d9dc507d8122b45bcf174742f80
Hoon Jo <pagaia@hotmail.com>
2022-09-27 09:46:07 +0900

    upt upper case

--- A/A.001/prometheus-stack-force-installer.sh
+++ A/A.001/prometheus-stack-force-installer.sh
@@ -3,7 +3,7 @@
 helm uninstall -n monitoring prometheus > /dev/null 2>&1
 helm uninstall -n monitoring prometheus-stack > /dev/null 2>&1
 kubectl -n monitoring delete pvc prometheus-prometheus-stack-kube-prom-prometheus-db-prometheus-prometheus-stack-kube-prom-prometheus-0 > /dev/null 2>&1
-echo "waiting for deleting prometheus componentes in 5 Seconds"; sleep 5
+echo "Waiting for deleting prometheus componentes in 5 Seconds"; sleep 5
 echo "Delete all prometheus components successfully!"
 echo "----------------------------------------------"
 
@@ -11,9 +11,9 @@ echo "Start to deploy prometheus operator with scraping 15s interval"
 echo "=============================================================="
 sh ~/_Lecture_prom_learning.kit/ch8/8.2/prometheus-stack-installer-15s.sh
 echo "--------------------------------------------------------------"
-echo "enable to srape control-plane componentes"
+echo "Enable to srape control-plane componentes"
 sh ~/_Lecture_prom_learning.kit/ch8/8.3/1.scraping-control-plane-enabled.sh
-echo "waiting for enabling prometheus componentes in 15 Seconds"; sleep 15
+echo "Waiting for enabling prometheus componentes in 15 Seconds"; sleep 15
 echo "--------------------------------------------------------------"
-echo "change etcd exposed port for insecure mode"
+echo "Change etcd exposed port for insecure mode"
 sh ~/_Lecture_prom_learning.kit/ch8/8.3/2.prometheus-stack-upgrader-15s.sh

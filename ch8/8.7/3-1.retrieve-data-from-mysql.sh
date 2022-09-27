462f3799a773006b005060d4562658515e65a8f8
Seongju Moon <seongju.moon@linux.com>
2022-09-28 04:19:27 +0900

    update sql path

--- ch8/8.7/3-1.retrieve-data-from-mysql.sh
+++ ch8/8.7/3-1.retrieve-data-from-mysql.sh
@@ -2,4 +2,4 @@
 
 MYSQL_HOST=$(kubectl get service mysql -o jsonpath="{.spec.clusterIP}")
 
-mysql -h $MYSQL_HOST -uroot -proot < db_scripts/retrieve_data
+mysql -h $MYSQL_HOST -uroot -proot < db_scripts/data/retrieve_data

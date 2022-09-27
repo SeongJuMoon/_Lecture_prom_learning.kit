842edba7bb8047cabc32474744b00e444726ff4f
Seongju Moon <seongju.moon@linux.com>
2022-09-28 05:05:06 +0900

    upt .cmd

---
+++ ch9/9.5/.cmd
@@ -0,0 +1,17 @@
+# 1. configure slack webhook url.
+./1.MUST-Run-to-change-Slack-URL.sh https://hooks.slack.com/services/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
+# 2. configure alertmanager 
+./2.prometheus-stack-upgrader-15s.sh
+# 3. add alerting rule. to monitor prometheus operator.
+kubectl apply -f 3.prometheusOperatorDown.yaml
+# 4. check prometheusrule on browser
+# shortcut http://192.168.1.10/targets 
+# check prometheus alert menu
+# 5. mutate deployment state.
+kubectl scale deploy -n monitoring prometheus-stack-kube-prom-operator --replicas=0
+# 6. check slack message.
+# you've seen alert
+# 7. mutate deployment state.
+kubectl scale deploy -n monitoring prometheus-stack-kube-prom-operator --replicas=1
+# 8. check slack message.
+# you've seen alert resolved

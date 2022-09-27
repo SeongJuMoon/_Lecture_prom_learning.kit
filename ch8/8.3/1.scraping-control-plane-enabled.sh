d77ff993c49f5a29dde8a16a782ad814f797c247
Hoon Jo <pagaia@hotmail.com>
2022-09-27 10:02:31 +0900

    chg messages

--- ch8/8.3/1.scraping-control-plane-enabled.sh
+++ ch8/8.3/1.scraping-control-plane-enabled.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
 # metricsBindAddress for kube-proxy 
+echo "Configure metrics-binder for kube-proxy"
 kubectl get configmap -n kube-system kube-proxy -o yaml       \
   | sed -r "s/(metricsBindAddress: ).*$/\1\"0.0.0.0:10249\"/" \
-  | kubectl replace -f -
-kubectl rollout restart daemonset kube-proxy -n kube-system
+  | kubectl replace -f - \
+  > /dev/null 2>&1
+kubectl rollout restart daemonset kube-proxy -n kube-system \
+  > /dev/null 2>&1
 
 # bind-address for kube-controller-manager's metrics 
+echo "Configure metrics-binder for kube-controller-manager"
 sed s,"- --bind-address=127.0.0.1","- --bind-address=0.0.0.0",g \
     -i /etc/kubernetes/manifests/kube-controller-manager.yaml
 # bind-address for kube-controller-manager's metrics 
+echo "Configure metrics-binder for kube-scheduler"
 sed s,"- --bind-address=127.0.0.1","- --bind-address=0.0.0.0",g \
     -i /etc/kubernetes/manifests/kube-scheduler.yaml
 # bind-address for kube-controller-manager's metrics 
+echo "Configure metrics-binder for etcd"
 sed s,"- --listen-metrics-urls=http://127.0.0.1:2381","- --listen-metrics-urls=http://0.0.0.0:2381",g \
     -i /etc/kubernetes/manifests/etcd.yaml
 
-echo 'etcd is rolling out!'
-echo 'the kubectl result will be slow or not response.'
-
-while [ "$(crictl ps | grep etcd-m-k8s)" == "" ]
-do
-echo "etcd rollout in progress "
-done
-echo "etcd rolled out successfully!"
+echo 'controller plane is rolling out!'
+  while [ "$(crictl ps | grep etcd-m-k8s)" == "" ]
+  do
+    echo "etcd rollout in progress "
+  done
+  echo "controller plane rolled out successfully!"

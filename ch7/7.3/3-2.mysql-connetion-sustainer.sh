5aba0d2cdab34eae04622940f1a13811cf549879
Hoon Jo <pagaia@hotmail.com>
2022-10-08 13:36:15 +0900

    chg sleep time+

--- ch7/7.3/3-2.mysql-connetion-sustainer.sh
+++ ch7/7.3/3-2.mysql-connetion-sustainer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for i in {1..300}
+for i in {1..150}
 do
-  mysql -h 192.168.1.73 -uroot -proot -e 'select sleep(299);' > /dev/null &
-  sleep 1
+  mysql -h 192.168.1.73 -uroot -proot -e 'select sleep(298);' > /dev/null &
+  sleep 2
 done

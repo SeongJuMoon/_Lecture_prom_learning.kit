77c8fc09b94e1dba7beb01faf08c8dd232349c1d
Hoon Jo <pagaia@hotmail.com>
2022-10-08 16:35:56 +0900

    add message

--- ch8/8.7/3-1.add-data-to-mysql.sh
+++ ch8/8.7/3-1.add-data-to-mysql.sh
@@ -3,3 +3,4 @@
 MYSQL_HOST=$(kubectl get service mysql -o jsonpath="{.spec.clusterIP}")
 
 mysql -h $MYSQL_HOST -uroot -proot < db_scripts/data/add_data
+echo "Create database(services) & table(MEMBERS) successfully"

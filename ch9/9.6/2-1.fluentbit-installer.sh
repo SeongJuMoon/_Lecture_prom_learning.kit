73d2d9b1da4ae45c48b994b916257e166405c226
Seongju Moon <seongju.moon@linux.com>
2022-10-15 14:18:03 +0900

    update lab

--- ch9/9.6/2-1.fluentbit-installer.sh
+++ ch9/9.6/2-1.fluentbit-installer.sh
@@ -4,6 +4,5 @@ helm install fluent-bit edu/fluent-bit \
 --set serviceMonitor.enabled="true" \
 --set serviceMonitor.selector.release="prometheus-stack" \
 --set serviceMonitor.namespace="monitoring" \
---set prometheusRule.enabled="true" \
 --namespace=logging \
 --create-namespace

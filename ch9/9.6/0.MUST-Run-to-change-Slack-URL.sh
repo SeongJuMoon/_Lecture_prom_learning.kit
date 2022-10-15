166a1a34d6f22551f0ad635c1186ff8220a25bfe
Seongju Moon <seongju.moon@linux.com>
2022-10-15 14:49:57 +0900

    update path

--- ch9/9.6/0.MUST-Run-to-change-Slack-URL.sh
+++ ch9/9.6/0.MUST-Run-to-change-Slack-URL.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed s,Slack-URL,$1,g -i ~/_Lecture_prom_learning.kit/ch9/9.5/prom-operator-config/add-alertmanager.yaml
+sed s,Slack-URL,$1,g -i ~/_Lecture_prom_learning.kit/ch9/9.5/prom-operator-config/prom-operator-config-merged.yaml

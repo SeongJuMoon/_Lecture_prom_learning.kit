6827628fecdae185861905afb9ca0052439051ce
Seongju Moon <seongju.moon@linux.com>
2023-02-11 20:26:30 +0900

    update docs for poweruser

--- A/A.003/.cmd
+++ A/A.003/.cmd
@@ -11,6 +11,11 @@ action label drop check
 # add patch for drop label heritage
 ./1.add-drop-label-patch.sh
 
+# check prometheus configuration 
+shortcut http://192.168.1.11/config
+
+# find heritage keyword via ctrl + F.
+
 # check prometheus ui graph, enter the below query.
 node_uname_info 
 node_procs_running
@@ -22,7 +27,12 @@ node_procs_running offset 10m
 kube_configmap_info offset 10m
 
 # restore prometheus configmap
-2.restore-drop-label-patch.sh
+./2.restore-drop-label-patch.sh
+
+# check prometheus configuration 
+shortcut http://192.168.1.11/config
+
+# find heritage keyword via ctrl + F.
 
 # check prometheus ui graph, enter the below query.
 node_uname_info 

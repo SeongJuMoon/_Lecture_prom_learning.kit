d462249404767e0072d6fa341a47adf85fdd4774
Seongju Moon <seongju.moon@linux.com>
2023-05-02 20:59:46 +0900

    WIP: make lab

--- A/A.005/.cmd
+++ A/A.005/.cmd
@@ -1,11 +1,16 @@
-# check 
-promtool check 
-
-# query 
-promtool query 
-
-# debug 
-promtool debug
-
-# tsdb 
-promtool tsdb 
+helm template prometheus edu/prometheus \
+--set pushgateway.enabled=false \
+--set alertmanager.enabled=false \
+--set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
+--set nodeExporter.tolerations[0].effect="NoSchedule" \
+--set nodeExporter.tolerations[0].operator="Exists" \
+--set nodeExporter.tolerations[1].key="node-role.kubernetes.io/control-plane" \
+--set nodeExporter.tolerations[1].effect="NoSchedule" \
+--set nodeExporter.tolerations[1].operator="Exists" \
+--set server.service.type="LoadBalancer" \
+--set server.global.scrape_interval="15s" \
+--set server.global.evaluation_interval="15s" \
+--set server.extraFlags[0]="web.enable-lifecycle" \
+--set server.extraFlags[1]="storage.tsdb.no-lockfile" \
+--namespace=monitoring \
+--create-namespace > manifest.yaml
--- A/A.005/promtool-installer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -LO https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
-tar -xzf prometheus-2.37.0.linux-amd64.tar.gz
-install -m 755 prometheus-2.37.0.linux-amd64/promtool /usr/local/bin
-rm -rf prometheus-2.37.0*

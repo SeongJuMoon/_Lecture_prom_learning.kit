93ab75544062c355a742914e515a27c1f5c77983
Seongju Moon <seongju.moon@linux.com>
2023-05-07 11:37:34 +0900

    fix error

--- A/A.005/.cmd
+++ A/A.005/.cmd
@@ -1,12 +1,20 @@
 # 1. deploy node exporter.
 k apply -f 1-1.node-exporter
 
-# check node-exporter.
-k apply -f 1-1.node-exporter
-k get po,svc -n monitoring
+# 2. check node-exporter via kubectl
+k get po,svc -n monitoring -l component=node-exporter
 
-# 2. deploy kube-state-metrics
+# 3. check node-exporter can response directly.
+curl http://192.168.1.10:9100/metrics # m-k8s
+curl http://192.168.1.101:9100/metrics # w1-k8s
+curl http://192.168.1.102:9100/metrics # w2-k8s
+curl http://192.168.1.103:9100/metrics # w3-k8s
+
+# 4. deploy kube-state-metrics
 k apply -f 1-2.kube-state-metrics
 
-# 3. deploy server
+# 5. check kube-state-metrics via kubectl
+k get po,svc -n monitoring -l component=kube-state-metrics
+
+# 6. deploy server
 k apply -f 2.prometheus-server

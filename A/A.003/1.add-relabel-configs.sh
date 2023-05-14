f749106f5a0dd8fe2cd6b7d92e16fd492a0dd033
Seongju Moon <seongju.moon@linux.com>
2023-05-14 10:05:05 +0900

    fix relabel configs error.

--- A/A.003/1.add-relabel-configs.sh
+++ A/A.003/1.add-relabel-configs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kubectl patch --patch-file=prometheus-server-configmaps/add-replace-n-keep.yaml -n monitoring cm/prometheus-server
+kubectl patch --patch-file=prometheus-server-configmaps/add-relabel-confings.yaml -n monitoring cm/prometheus-server

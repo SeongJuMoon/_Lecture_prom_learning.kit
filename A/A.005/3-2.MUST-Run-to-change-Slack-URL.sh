98eae45e52194053e2bc5bfb5512580312cb9c7a
Seongju Moon <seongju.moon@linux.com>
2024-01-16 19:47:11 +0900

    rewind structure.

--- A/A.005/3-2.MUST-Run-to-change-Slack-URL.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sed s,Slack-URL,$1,g -i ~/_Lecture_prom_learning.kit/A/A.005/4.alertmanager/configmap.yaml
---
+++ A/A.005/5-1.cleanup-native-prometheus.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$HOME/_Lecture_prom_learning.kit/A/A.005"
+
+echo "Delete alert-target-nginx"
+kubectl delete -f $BASE_DIR/4-2.alert-target-nginx/
+
+echo "Delete alertmanager"
+kubectl delete -f $BASE_DIR/3-2.alertmanager.yaml
+
+echo "Delete kube-state-metrics"
+kubectl delete -f $BASE_DIR/2-2.kube-state-metrics.yaml
+
+echo "Delete node-exporter"
+kubectl delete -f $BASE_DIR/2-1.prometheus-server.yaml
+
+echo "Delete prometheus-server and config also namespace."
+kubectl delete -f $BASE_DIR/1.prometheus-server.yaml
+
+echo "delete success all native-prometheus apps."

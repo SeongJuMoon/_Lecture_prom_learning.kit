83979b193b7acb6eb0a16758a96c0e523c5c6c63
Seongju Moon <seongju.moon@linux.com>
2024-01-16 20:55:55 +0900

    WIP .cmd write

--- A/A.005/.cmd
+++ A/A.005/.cmd
@@ -5,83 +5,41 @@
 
 
 # 0. run script to initialize control-plane metric scape enabled.
-# this script need to a lot of time.
-# please stay moment for processing.
-./0.scraping-control-plane-enabled.sh
 
-# 1. create namespace for prometheus environment.
-k create ns monitoring
+# 1-1 deploy prometheus-server
+k apply -f 1-1.prometheus-server.yaml
 
-# 2. deploy server
-k apply -f 1.prometheus-server/
+# 1-2 check prometheus-server is available
+watch -n 2 kubectl get pods -n monitoring
 
-# 3. check prometheus-server via kubectl.
-k get po,svc -n monitoring -l component=server
+# 1-3 check pods describe for what's matter in prometheus-server.
+k describe -n monitoring pods prometheus-server-<tab>
+#   MountVolume.SetUp failed for volume "config-volume" : configmap "prometheus-server" not found
 
-# 4. check prometheus web ui go to config
-# <shortcut>: http://192.168.1.11/config
+# 1-4 add deploy initial prometheus-configmap.
+k apply -f 1-2-prometheus-server-configmap.yaml
 
-# 5. check metric on prometheus web ui.
-# <shortcut>: http://192.168.1.11/graph
-# search metrics: node_cpu_seconds_total # not found
-# search metrics: kube_deployment_status_replicas_available # not found
-# search metrics: kubeproxy_sync_proxy_rules_iptables_total # found
+# 1-5 check prometheus-server is available
+watch -n 2 kubectl get pods -n monitoring
 
-# 6. deploy node exporter.
-k apply -f 2.node-exporter/
+# 1-6 rollout prometheus-server deployment.
+k rollout restart -n monitoring deploy prometheus-server
 
-# 7. check node-exporter via kubectl.
-k get po,svc -n monitoring -l component=node-exporter
+# 2-1 deploy node-exporter
+k apply -f 2-1.node-exporter.yaml
 
-# 8.check the targets status on your browser(http://192.168.1.11/targets).
+# 2-2 check node-exporter metrics via curl
+curl http://192.168.1.10:9100/metrics
+curl http://192.168.1.102:9100/metrics
 
-# 9. check metric on prometheus web ui (node-exporter).
-# <shortcut>: http://192.168.1.11/graph
-# search metrics: node_cpu_seconds_total # found
+# 2-3 deploy configmap for scraping the metrics
+k apply -f 2-2.config-w-annotations.yaml
 
-# 10. deploy kube-state-metrics.
-k apply -f 3.kube-state-metrics/
+# 2-4 deploy kube-state-metrics 
+k apply -f 2-3.kube-state-metrics.yaml
 
-# 11. check kube-state-metrics via kubectl.
-k get po,svc -n monitoring -l app.kubernetes.io/name=kube-state-metrics
+# 3. recording-rule 설정
+kubectl patch configmap prometheus-server -n monitoring --patch-file 3.prometheus-recording.rules-container-memory-topk3.yaml
 
-# 12.check the targets status on your browser(http://192.168.1.11/targets).
-# <shortcut>: http://192.168.1.11/targets
-
-# 13. check metric on prometheus web ui (kube-state-metrics).
-# <shortcut>: http://192.168.1.11/graph
-# search metrics: kube_deployment_status_replicas_available # found
-
-# 14. check alertrule on prometheus web ui
-# <shortcut>: http://192.168.1.11/alerts
-
-# 15. register slack receiver on alertmanager 
-# change slack API Address
-./3-2.MUST-Run-to-change-Slack-URL.sh "https://hooks.slack.com/services/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"
-
-# 16. deploy node exporter.
-k apply -f 4.alertmanager/
-
-# 17. check alertmanager via kubectl
-k get po,svc -n monitoring -l component=alertmanager
-
-# 18. deploy sample-app(nginx) for trigger alert test.
-k apply -f 5.test-alertmanager-sendalert/1.nginx-w-exporter/
-
-# 19 check application deploy successfully.
-k get po,svc -l app=nginx
-
-# 20. Add alert-rule for trigger alert test.
-k patch configmap prometheus-server -n monitoring --patch-file 5.test-alertmanager-sendalert/2.nginx-status.alerting.rules.yaml
-
-# 21. check alertrule on prometheus web ui
-# <shortcut>: http://192.168.1.11/alerts
-
-# 22 adjust replicas to trigger alert test
-k scale deploy nginx --replicas=0
-
-# 23 rollback replicas to snooze alert test
-k scale deploy nginx --replicas=2
-
-# 24 cleanup lab
-./6.cleanup-native-prometheus.sh
+# 4 cleanup lab
+./4.cleanup-native-prometheus.sh

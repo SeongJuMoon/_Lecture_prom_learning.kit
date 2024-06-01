19a27752eb644c1fbe6628a2fd7142a42564057d
Hoon Jo <pagaia@hotmail.com>
2024-06-01 22:42:13 +0900

    rearrgane all

---
+++ ch2/2.5/.cmd
@@ -0,0 +1,5 @@
+# deploy native prometheus thru helm which encap by shell
+./.prometheus-installer.sh
+
+# if you want to uninstall it, run this command 
+~/_Lecture_prom_learning.kit/A/A.001/native-prometheus-uninstaller.sh

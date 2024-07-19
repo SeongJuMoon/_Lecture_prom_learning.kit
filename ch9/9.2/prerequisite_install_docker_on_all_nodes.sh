a0140e65fd3c5cd7cef921bfca1bf3b00d37b23e
Hoon Jo <pagaia@hotmail.com>
2024-07-19 10:33:17 +0900

    upt prerequisite_install_docker_on_all_nodes.sh

--- ch9/9.2/prerequisite_install_docker_on_all_nodes.sh
+++ ch9/9.2/prerequisite_install_docker_on_all_nodes.sh
@@ -7,6 +7,7 @@ for (( i=1; i<=$NODE_COUNT; i++ ));
   do
   TARGET="192.168.1.10$i"
   echo "[Step $i/3] Installing docker on $TARGET"
-  sshpass -p vagrant ssh -o stricthostkeychecking=no root@$TARGET 'bash apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y  > /dev/null 2>&1'
+  sshpass -p vagrant ssh -o stricthostkeychecking=no root@$TARGET "echo apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y  > /tmp/install_docker.sh"
+  sshpass -p vagrant ssh root@$TARGET bash /tmp/install_docker.sh > /dev/null 2>&1
   done
 echo "Successfully completed"

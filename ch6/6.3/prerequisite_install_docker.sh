1652ff0437dde368fd375e4ab23f937f84f38fb6
Hoon Jo <pagaia@hotmail.com>
2024-07-19 09:27:36 +0900

    rmv prerequisite_install_docker.sh due to dup

--- ch6/6.3/prerequisite_install_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Docker version 
-docker_V='5:24.0.6-1~ubuntu.22.04~jammy' 
-
-# install & enable docker 
-apt-get update 
-apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y 
-

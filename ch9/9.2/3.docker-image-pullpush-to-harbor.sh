16b9011c2d81fdbed760e7881dd8e663b8730ecd
Hoon Jo <pagaia@hotmail.com>
2024-07-19 10:51:04 +0900

    rmv prerequisite_install_docker_on_all_nodes.sh

--- ch9/9.2/3.docker-image-pullpush-to-harbor.sh
+++ ch9/9.2/3.docker-image-pullpush-to-harbor.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Docker version 
+docker_V='5:24.0.6-1~ubuntu.22.04~jammy' 
+
+# install & enable docker 
+apt-get update 
+apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y 
+
+# upload to harbor 
 docker login 192.168.1.92 --username admin --password admin
 docker pull nginx
 docker tag nginx 192.168.1.92/library/nginx

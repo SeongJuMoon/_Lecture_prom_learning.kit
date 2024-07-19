317c17e0b9c47e2ba1b56c90f64a796a85c6db0f
Hoon Jo <pagaia@hotmail.com>
2024-07-19 16:17:43 +0900

    upt docker stdout message

--- ch9/9.2/3.docker-image-pullpush-to-harbor.sh
+++ ch9/9.2/3.docker-image-pullpush-to-harbor.sh
@@ -3,9 +3,9 @@
 # Docker version 
 docker_V='5:24.0.6-1~ubuntu.22.04~jammy' 
 
-# install & enable docker 
-apt-get update 
-apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y 
+# install & enable docker (bg)
+apt-get update > /dev/null 2>&1
+apt-get install docker-ce=$docker_V docker-ce-cli=$docker_V -y > /dev/null 2>&1
 
 # upload to harbor 
 docker login 192.168.1.92 --username admin --password admin

c5509dfc23fdc1b6d00f63aea5eb6ec08f4a5017
Seongju Mun <seongju.moon@linux.com>
2024-08-03 19:40:20 +0900

    fix shell

--- A/A.007/4-1.generate-alertmanager-config.sh
+++ A/A.007/4-1.generate-alertmanager-config.sh
@@ -5,5 +5,5 @@ cp opsgenie-notifier.yaml.tpl 4-2.opsgenie-notifier.yaml
 # Prompt for the API token
 read -p 'Please enter your Opsgenie API Key: ' API_KEY
 # Replace 'api_token' placeholder in the copied YAML file
-sed -i -e "s,api-key,$API_KEY," 4-1.opsgenie-notifier.yaml
+sed -i -e "s,api-key,$API_KEY," 4-2.opsgenie-notifier.yaml
 echo 'Generate Opsgenie notifier configuration successfully.'

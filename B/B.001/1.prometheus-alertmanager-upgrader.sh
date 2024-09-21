18db727d417d41c174519aba47a49b4685ac0c07
Seongju Mun <seongju.moon@linux.com>
2024-09-21 12:53:29 +0900

    sync to 6.5.

--- B/B.001/1.prometheus-alertmanager-upgrader.sh
+++ B/B.001/1.prometheus-alertmanager-upgrader.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 helm upgrade --install prometheus edu/prometheus \
+--namespace=monitoring \
+--create-namespace \
 --set pushgateway.enabled=false \
 --set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
 --set nodeExporter.tolerations[0].effect="NoSchedule" \
@@ -10,10 +12,8 @@ helm upgrade --install prometheus edu/prometheus \
 --set nodeExporter.tolerations[1].operator="Exists" \
 --set server.service.type="LoadBalancer" \
 --set alertmanager.service.type="LoadBalancer" \
---set alertmanager.service.loadBalancerIP="192.168.1.98" \
+--set alertmanager.service.loadBalancerIP="192.168.1.65" \
 --set server.global.scrape_interval="15s" \
 --set server.global.evaluation_interval="15s" \
 --set server.extraFlags[0]="web.enable-lifecycle" \
---set server.extraFlags[1]="storage.tsdb.no-lockfile" \
---namespace=monitoring \
---create-namespace
+--set server.extraFlags[1]="storage.tsdb.no-lockfile"
